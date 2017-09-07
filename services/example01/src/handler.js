503cc948006627325095aebf4730f9ac4ca7bcd9
Yannick Huard <mail@yannickhuard.com>
2017-09-07 14:42:52 +0200

    Dynamic path

--- services/example01/src/handler.js
+++ services/example01/src/handler.js
@@ -4,12 +4,14 @@ import getMessage from './message';
 export const hello = (event, context, callback) => {
   console.log(event); // Contains incoming request data (e.g., query params, headers and more)
 
+  const { name } = event.pathParameters || { name: 'World' };
+
   const response = {
     statusCode: 200,
     headers: {
       'x-custom-header': 'My Header Value',
     },
-    body: JSON.stringify({ message: getMessage('World') }),
+    body: JSON.stringify({ message: getMessage(name) }),
   };
 
   callback(null, response);
