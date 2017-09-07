4277ecd8e54462f23fabe4aac074d3244d83a791
Yannick Huard <mail@yannickhuard.com>
2017-09-07 15:01:17 +0200

    Add tests

---
+++ services/example01/test/message.test.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+import getMessage from '../src/message';
+
+test(`getMessage('World') to return 'Hello World!'`, () => {
+  expect(getMessage('World')).toBe('Hello World!');
+});
