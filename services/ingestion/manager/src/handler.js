6fd2f2eb8b3b6c759744747b9f4c3f0d3c72cbad
Yannick Huard <mail@yannickhuard.com>
2017-09-14 12:04:36 +0200

    Publish message to SNS topic

--- services/ingestion/manager/src/handler.js
+++ services/ingestion/manager/src/handler.js
@@ -1,16 +1,47 @@
 /* eslint-disable import/prefer-default-export, no-console */
+const AWS = require('aws-sdk');
+
 export const onCreate = (event, context, callback) => {
   // Contains incoming request data (e.g., query params, headers and more)
   console.log('event', event);
   console.log('context', context);
 
-  const response = {
-    statusCode: 200,
-    headers: {
-      'x-custom-header': 'My Header Value',
-    },
-    body: JSON.stringify({ context, event }),
+  // Some logic here
+
+  // Send SNS message
+  const sns = new AWS.SNS();
+  const endpointArn = 'arn:aws:sns:eu-central-1:491621799026:test-huartya';
+  const payload = {
+    default: JSON.stringify({
+      test: 'Hello',
+      records: event.Records,
+    }),
   };
 
-  callback(null, response);
+  sns.publish(
+    {
+      Message: JSON.stringify(payload),
+      MessageStructure: 'json',
+      TargetArn: endpointArn,
+    },
+    (err, data) => {
+      if (err) {
+        console.log(err.stack);
+        return;
+      }
+
+      console.log('push sent');
+      console.log(data);
+
+      const response = {
+        statusCode: 200,
+        headers: {
+          'x-custom-header': 'My Header Value',
+        },
+        body: JSON.stringify({ data }),
+      };
+
+      callback(null, response);
+    }
+  );
 };
