7552cf81c409d4fe0e774f47f64b7a1c2287bea2
Yannick Huard <mail@yannickhuard.com>
2017-09-20 11:58:51 +0200

    feat(etl): comply with the expected output json - EUBFR-22

--- services/ingestion/etl/budg/csv/src/handler.js
+++ services/ingestion/etl/budg/csv/src/handler.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/prefer-default-export, no-console */
-import saveToDB from '@eubfr/dynamodb-helpers/save';
+import { saveProject } from '@eubfr/dynamodb-helpers/save';
 
 const path = require('path');
 const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
@@ -48,37 +48,85 @@ export const parseCsv = (event, context, callback) => {
        * Map fields
        */
 
+      // Preprocess timeframe
+      let timeframeFrom = null;
+      let timeframeTo = null;
+
+      if (record['Timeframe start'].indexOf(' to ') !== -1) {
+        const timeframe = (record['Timeframe start'] || '').split(' to ');
+
+        if (Array.isArray(timeframe) && timeframe.length === 2) {
+          [timeframeFrom, timeframeTo] = timeframe;
+        }
+      } else {
+        timeframeFrom = record['Timeframe start'];
+        timeframeTo = record['Timeframe end'];
+      }
+
+      // Preprocess related links
+      const links = (record['Related links'] || '')
+        .split(';')
+        .map(link => {
+          const matches = link.match(/<a .*href="(.*)".*>(.*)<\/a>/i);
+
+          if (Array.isArray(matches) && matches.length === 3) {
+            return {
+              url: matches[1],
+              label: matches[2],
+            };
+          }
+
+          return null;
+        })
+        .filter(link => link !== null);
+
+      // Preprocess project locations
+      const latArray = record['Project location latitude'].split(';');
+      const longArray = record['Project location longitude'].split(';');
+      const projectLocations = record['Project country(ies)']
+        .split(';')
+        .map((country, index) => ({
+          name: country,
+          geolocation: {
+            lat: (Array.isArray(latArray) && latArray[index]) || null,
+            long: (Array.isArray(longArray) && longArray[index]) || null,
+          },
+        }));
+
       // Map the fields
       const data = {
-        source: message.object.key,
+        creation_date: message.eventTime, // already ISO-8601
+        source: {
+          producer: message.userIdentity,
+          object_key: message.object.key,
+        },
         title: record.Name,
+        cover_image: record.Visual,
         programme_name: record['Programme name'],
         description: record['Project description'],
         results: record.Results,
-        ec_priorities: record['EC’s priorities'].split(','),
-        coordinators: record.Coordinators.split(','),
+        ec_priorities: record['EC’s priorities'].split(';'),
+        coordinators: record.Coordinators.split(';'),
         eu_budget_contribution: record['EU Budget contribution'],
-        partners: record.Partners.split(','),
-        project_locations: [
-          {
-            name: record['Project country(ies)'],
-            geolocation: {
-              lat: record['Project location latitude'],
-              long: record['Project location longitude'],
-            },
-          },
-        ],
+        partners: record.Partners.split(';'),
+        project_locations: projectLocations,
         timeframe: {
-          from: record['Timeframe start'],
-          to: record['Timeframe end'],
+          from:
+            timeframeFrom &&
+            new Date(parseInt(timeframeFrom, 10) * 1000).toISOString(),
+          to:
+            timeframeTo &&
+            new Date(parseInt(timeframeTo, 10) * 1000).toISOString(),
         },
+        project_website: record['Project webpage'],
+        related_links: links,
       };
 
       /*
        * Save to DB
        */
 
-      saveToDB(dynamo, process.env.TABLE, data, err => {
+      saveProject(dynamo, process.env.TABLE, data, err => {
         if (err) {
           console.log(err);
         }
