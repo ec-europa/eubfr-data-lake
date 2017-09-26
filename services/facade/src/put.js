4c184dd65cdca7e50d62a6e6d02d06961698e193
Kalin Chernev <kalin.chernev@gmail.com>
2017-09-26 10:09:22 +0200

    feat(facade): include boilerplate signed upload to s3

--- services/facade/src/put.js
+++ services/facade/src/put.js
@@ -1,19 +1,30 @@
+import AWS from 'aws-sdk'; // eslint-disable-line import/no-extraneous-dependencies
+
+const s3 = new AWS.S3({ signatureVersion: 'v4' });
+
 /* eslint-disable import/prefer-default-export, no-console */
 export const handler = (event, context, callback) => {
-  console.log(`---------------------------------------------------`);
-  console.log(`Logging event`);
-  console.log(event);
-  console.log(`---------------------------------------------------`);
-  console.log(`Logging context`);
-  console.log(context);
-
-  const response = {
-    statusCode: 200,
-    headers: {
-      'x-custom-header': 'My Header Value',
+  const params = {
+    Bucket: 'eubfr-dev',
+    Fields: {
+      foo: 'bar',
     },
-    body: JSON.stringify({ message: 'Create or update projects' }),
   };
 
-  callback(null, response);
+  s3.createPresignedPost(params, (err, data) => {
+    if (err) {
+      console.error('Presigning post data encountered an error', err);
+    }
+
+    const response = {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+        'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
+      },
+      body: JSON.stringify(data),
+    };
+
+    callback(null, response);
+  });
 };
