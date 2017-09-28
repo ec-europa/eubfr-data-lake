12397aa7fd17f767bf1d67a5f3ece31d3a27bcdb
Yannick Huard <mail@yannickhuard.com>
2017-09-28 15:19:27 +0200

    feat(meta-index): add meta index - EUBFR-28 (#11)
    * feat(meta-index): add meta index - EUBFR-28
    
    * Save to DB
    
    * Add delete handler
    
    * Fix typo

---
+++ services/storage/meta-index/src/handler.js
@@ -0,0 +1,107 @@
+/* eslint-disable import/prefer-default-export, no-console */
+import AWS from 'aws-sdk'; // eslint-disable-line import/no-extraneous-dependencies
+
+export const onObjectCreated = (event, context, callback) => {
+  /*
+   * Some checks here before going any further
+   */
+
+  if (!event.Records) {
+    return callback('No record');
+  }
+
+  // Only work on the first record
+  const snsRecord = event.Records[0];
+
+  // Was the lambda triggered correctly? Is the file extension supported? etc.
+  if (!snsRecord || snsRecord.EventSource !== 'aws:sns') {
+    return callback('Bad record');
+  }
+
+  /*
+   * Extract information from the event
+   */
+
+  // Extract S3 record
+  const s3record = JSON.parse(snsRecord.Sns.Message).Records[0];
+
+  // Retrieve file meta
+  const s3 = new AWS.S3();
+
+  return s3.headObject(
+    {
+      Bucket: s3record.s3.bucket.name,
+      Key: s3record.s3.object.key,
+    },
+    (err, data) => {
+      if (err) return callback(err); // an error occurred
+
+      // Save record
+      const documentClient = new AWS.DynamoDB.DocumentClient({
+        apiVersion: '2012-08-10',
+      });
+
+      const params = {
+        TableName: process.env.TABLE,
+        Item: {
+          computed_key: s3record.s3.object.key,
+          event_time: s3record.eventTime,
+          producer_id: s3record.userIdentity.principalId,
+          content_type: data.ContentType,
+          last_modified: data.LastModified.toISOString(), // ISO-8601 date
+          content_length: data.ContentLength,
+          metadata: data.Metadata,
+        },
+      };
+
+      return documentClient.put(params, dynamoErr => {
+        if (dynamoErr) return callback(dynamoErr);
+
+        return callback(null, 'All fine');
+      });
+    }
+  );
+};
+
+export const onObjectRemoved = (event, context, callback) => {
+  /*
+   * Some checks here before going any further
+   */
+
+  if (!event.Records) {
+    return callback('No record');
+  }
+
+  // Only work on the first record
+  const snsRecord = event.Records[0];
+
+  // Was the lambda triggered correctly? Is the file extension supported? etc.
+  if (!snsRecord || snsRecord.EventSource !== 'aws:sns') {
+    return callback('Bad record');
+  }
+
+  /*
+   * Extract information from the event
+   */
+
+  // Extract S3 record
+  const s3record = JSON.parse(snsRecord.Sns.Message).Records[0];
+
+  // Delete record
+  const documentClient = new AWS.DynamoDB.DocumentClient({
+    apiVersion: '2012-08-10',
+  });
+
+  const params = {
+    TableName: process.env.TABLE,
+    Key: {
+      computed_key: s3record.s3.object.key,
+    },
+  };
+
+  return documentClient.delete(params, dynamoErr => {
+    if (dynamoErr) return callback(dynamoErr);
+
+    return callback(null, 'All fine');
+  });
+};
