1d930d68bb075571d3da674f0b70234246db7748
Kalin Chernev <kalin.chernev@gmail.com>
2017-10-06 17:26:00 +0200

    Correct tests

--- services/ingestion/cleaner/test/integration/events/onObjectRemoved.spec.js
+++ services/ingestion/cleaner/test/integration/events/onObjectRemoved.spec.js
@@ -8,7 +8,7 @@ import eventEubfr from '../../stubs/events/onObjectRemoved/event-eubfr.json';
 
 const handler = promisify(onObjectRemoved);
 
-describe(`Function onObjectRemoved in "@eubfr/ingestion-cleaner"`, () => {
+describe(`Function onObjectRemoved in "@eubfr/ingestion-cleaner" when S3 service works well`, () => {
   beforeAll(() => {
     AWS.mock('S3', 'deleteObject', (params, callback) => {
       callback(null, 'Successfully deleted a file from the S3 storage');
@@ -25,13 +25,44 @@ describe(`Function onObjectRemoved in "@eubfr/ingestion-cleaner"`, () => {
   });
 
   test('The function removes an object from S3 with the actual event stub', () => {
+    process.env.BUCKET = 'foo';
     const event = eventEubfr;
     const context = {};
 
     const result = handler(event, context);
     result.then(response => {
-      // Either a null, error or a rejected promise because of bad input.
       expect(response).toBe(`object removed`);
     });
+  });
+});
+
+describe(`Function onObjectRemoved in "@eubfr/ingestion-cleaner" when S3 service fails`, () => {
+  beforeAll(() => {
+    AWS.mock('S3', 'deleteObject', (params, callback) => {
+      callback('S3 failure');
+    });
+  });
+
+  afterEach(() => {
+    // ensure that the environment is not left over a test
+    delete process.env.BUCKET;
+  });
+
+  afterAll(() => {
+    AWS.restore('S3', 'deleteObject');
+  });
+
+  test('The function fails to remove an object from S3 because of service failure', () => {
+    process.env.BUCKET = 'foo';
+    const event = eventEubfr;
+    const context = {};
+
+    const result = handler(event, context);
+    result
+      .then(response => {
+        // Either a null, error or a rejected promise because of bad input.
+        expect(response).toBeFalsy();
+      })
+      .catch(e => expect(e).toBe('S3 failure'));
   });
 });
