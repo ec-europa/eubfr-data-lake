7db7e785d573baaa81c5e4d171a018cbbcc66b92
Kalin Chernev <kalin.chernev@gmail.com>
2017-10-09 11:02:35 +0200

    Simplify structure assertions

--- services/ingestion/etl/budg/csv/test/unit/lib/transform.spec.js
+++ services/ingestion/etl/budg/csv/test/unit/lib/transform.spec.js
@@ -12,37 +12,8 @@ describe(`DG BUDG CSV transformer`, () => {
     expect(mapper).toThrow();
   });
 
-  test(`Timeframe information is transformed into a single object field`, () => {
-    expect(result.timeframe).toBeDefined();
-  });
-
-  test(`Timeframe's "from" field is present`, () => {
-    expect(result.timeframe.from).toBeDefined();
-  });
-
-  test(`Timeframe's "to" field is present`, () => {
-    expect(result.timeframe.to).toBeDefined();
-  });
-
-  test(`"Name" source field is transformed to "title"`, () => {
-    expect(result.title).toBeDefined();
-    expect(result.title).toBe(`Central European Green Corridors`);
-  });
-
-  test(`"Related links" source field is transformed to "related_links"`, () => {
-    expect(result.related_links).toBeDefined();
-    expect(result.related_links).toBeTruthy();
-  });
-
-  test(`"Related links" has URL values correctly split`, () => {
-    expect(result.related_links[0].url).toBeDefined();
-    expect(result.related_links[0].label).toBeDefined();
-  });
-
-  test(`"Project location latitude" and "Project location longitude" move to "project_locations"`, () => {
-    expect(result.project_locations).toBeDefined();
-    // "AT;HR;DE;SK;SI" => there should be 5 locations.
-    expect(result.project_locations.length).toBe(5);
+  test(`Produces correct JSON output structure`, () => {
+    expect(result).toMatchSnapshot();
   });
 
   test(`Project location information is correctly mapped`, () => {
