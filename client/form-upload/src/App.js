8a6a32356b5da9e7a7b81af23a3bb86db685a8ba
Kalin Chernev <kalin.chernev@gmail.com>
2017-10-10 15:41:09 +0200

    feat(form-upload): linter rules

--- client/form-upload/src/App.js
+++ client/form-upload/src/App.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <p className="App-intro">
+      To get, edit <code>src/App.js</code> and save to reload.
+    </p>
+  </div>
+);
 
 export default App;
