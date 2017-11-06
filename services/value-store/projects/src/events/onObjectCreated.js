63ed2cd2252bbe32e7f4319bf64b3d8f0e955f7b
Kalin Chernev <kalin.chernev@gmail.com>
2017-11-06 12:29:19 +0100

    ES domain in code

--- services/value-store/projects/src/events/onObjectCreated.js
+++ services/value-store/projects/src/events/onObjectCreated.js
@@ -1,8 +1,4 @@
 import AWS from 'aws-sdk'; // eslint-disable-line import/no-extraneous-dependencies
-import through2 from 'through2';
-import split2 from 'split2';
-import SaveStream from '../lib/SaveStream';
-import deleteProjects from '../lib/deleteProjects';
 
 export const handler = (event, context, callback) => {
   /*
@@ -30,59 +26,6 @@ export const handler = (event, context, callback) => {
 
   // Retrieve file meta
   const s3 = new AWS.S3();
-
-  // Save record
-  const documentClient = new AWS.DynamoDB.DocumentClient({
-    apiVersion: '2012-08-10',
-    convertEmptyValues: true,
-  });
-
-  const { TABLE } = process.env;
-
-  return s3
-    .headObject({
-      Bucket: s3record.s3.bucket.name,
-      Key: s3record.s3.object.key,
-    })
-    .promise()
-    .then(data => {
-      deleteProjects({
-        documentClient,
-        table: TABLE,
-        key: s3record.s3.object.key,
-      }).then(() => {
-        const saveStream = new SaveStream({
-          objectMode: true,
-          documentClient,
-          table: TABLE,
-        });
-
-        return s3
-          .getObject({
-            Bucket: s3record.s3.bucket.name,
-            Key: s3record.s3.object.key,
-          })
-          .createReadStream()
-          .pipe(split2(JSON.parse))
-          .pipe(
-            through2.obj((chunk, enc, cb) => {
-              // Enhance item to save
-              const item = Object.assign(
-                {
-                  computed_key: s3record.s3.object.key,
-                  producer_id: s3record.userIdentity.principalId,
-                  last_modified: data.LastModified.toISOString(), // ISO-8601 date
-                },
-                chunk
-              );
-
-              return cb(null, item);
-            })
-          )
-          .pipe(saveStream);
-      });
-    })
-    .catch(err => callback(err));
 };
 
 export default handler;
--- services/value-store/projects/src/events/onObjectRemoved.js
+++ services/value-store/projects/src/events/onObjectRemoved.js
@@ -1,5 +1,4 @@
 import AWS from 'aws-sdk'; // eslint-disable-line import/no-extraneous-dependencies
-import deleteProjects from '../lib/deleteProjects';
 
 export const handler = (event, context, callback) => {
   /*
@@ -24,25 +23,6 @@ export const handler = (event, context, callback) => {
 
   // Extract S3 record
   const s3record = JSON.parse(snsRecord.Sns.Message).Records[0];
-
-  /*
-   * Get all records related to the S3 object
-   */
-  const documentClient = new AWS.DynamoDB.DocumentClient({
-    apiVersion: '2012-08-10',
-    convertEmptyValues: true,
-  });
-
-  const { TABLE } = process.env;
-
-  /*
-   * Delete existing records
-   */
-  return deleteProjects({
-    documentClient,
-    table: TABLE,
-    key: s3record.s3.object.key,
-  });
 };
 
 export default handler;
