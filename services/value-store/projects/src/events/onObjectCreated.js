51fe4a17a4c2df188ba49c794220f9221a9dfb14
Kalin Chernev <kalin.chernev@gmail.com>
2017-11-08 10:35:26 +0100

    Ping from lambda function

--- services/value-store/projects/src/events/onObjectCreated.js
+++ services/value-store/projects/src/events/onObjectCreated.js
@@ -1,35 +1,38 @@
 import AWS from 'aws-sdk'; // eslint-disable-line import/no-extraneous-dependencies
 
+import elasticsearch from 'elasticsearch';
+import connectionClass from 'http-aws-es';
+
 export const handler = (event, context, callback) => {
   // API to work with
   const { API } = process.env;
 
-  if (!event.Records) {
-    return callback('No record');
-  }
+  const options = {
+    hosts: [API],
+    connectionClass,
+    log: 'trace',
+    index: 'projects',
+    // httpOptions: {}, // set httpOptions on aws-sdk's request. default to aws-sdk's config.httpOptions
+  };
 
-  // Only work on the first record
-  const snsRecord = event.Records[0];
+  const client = elasticsearch.Client(options);
 
-  // Was the lambda triggered correctly? Is the file extension supported? etc.
-  if (!snsRecord || snsRecord.EventSource !== 'aws:sns') {
-    return callback('Bad record');
-  }
-
-  /*
-   * Extract information from the event
-   */
-
-  // Extract S3 record
-  const s3record = JSON.parse(snsRecord.Sns.Message).Records[0];
-
-  const s3 = new AWS.S3();
+  client.ping(
+    {
+      // ping usually has a 3000ms timeout
+      requestTimeout: 1000,
+    },
+    error => {
+      if (error) {
+        console.trace(error);
+      } else {
+        console.log('All is well');
+      }
+    }
+  );
 
   // Retrieve file meta
-  return s3.headObject({
-    Bucket: s3record.s3.bucket.name,
-    Key: s3record.s3.object.key,
-  });
+  return {};
 };
 
 export default handler;
