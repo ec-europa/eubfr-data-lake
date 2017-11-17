f0d1a6776f5f35bb514b270be6a5dee84ed6661b
Radu Chiriac <contact@raduchiriac.me>
2017-11-17 10:43:27 +0100

    Make usage of elasticsearch

--- demo/website/src/App.js
+++ demo/website/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ProjectsList from './components/ProjectsList';
+import elasticsearch from 'elasticsearch';
 
 import './App.css';
 
 const App = () => (
   <div className="App">
     <h1>EUBFR Website</h1>
-    <ProjectsList />
+    <ProjectsList elasticsearch={elasticsearch} />
   </div>
 );
 
