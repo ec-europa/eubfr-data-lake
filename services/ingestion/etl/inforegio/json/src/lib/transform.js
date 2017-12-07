005f495b0e20b03841831f2c1cfa76d29f4fa252
Romain Emery <emery.romain@gmail.com>
2017-12-07 12:03:56 +0100

    refactor(etl): new inforegio structure (#53)

--- services/ingestion/etl/inforegio/json/src/lib/transform.js
+++ services/ingestion/etl/inforegio/json/src/lib/transform.js
@@ -6,32 +6,32 @@
 const formatDate = date => {
   if (!date) return null;
   const d = date.split(/\//);
-  if (d.length !== 3) return null;
+  if (d === null || d.length !== 3) return null;
   if (d[2].length === 2) d[2] = `20${d[2]}`;
   return new Date(d[2], d[1] - 1, d[0]).toISOString();
 };
 
 const getAddress = record => {
   let address = '';
-  if (record['beneficiary address']) {
-    address += record['beneficiary address'];
+  if (record.Beneficiary_address) {
+    address += record.Beneficiary_address;
   }
-  if (record['beneficiary post code']) {
+  if (record.Beneficiary_Post_Code) {
     if (address !== '') address += ', ';
-    address += record['beneficiary post code'];
+    address += record.Beneficiary_Post_Code;
   }
-  if (record['beneficiary city']) {
+  if (record.Beneficiary_City) {
     if (address !== '') address += ', ';
-    address += record['beneficiary city'];
+    address += record.Beneficiary_City;
   }
   return address;
 };
 
 const getProjectWebsite = record => {
-  if (record.url && typeof record.url === 'object') {
-    return record.url[0];
-  } else if (record.url && typeof record.url === 'string') {
-    return record.url;
+  if (record.URL && typeof record.URL === 'object') {
+    return record.URL[0];
+  } else if (record.URL && typeof record.URL === 'string') {
+    return record.URL;
   }
   return '';
 };
@@ -40,7 +40,7 @@ const formatBudget = budget => {
   if (!budget) return null;
   const b = budget.split(' ');
 
-  if (b.length < 2) return 0;
+  if (b === null || b.length < 2) return 0;
 
   let s = '';
   for (let i = 1; i < b.length; i += 1) {
@@ -55,21 +55,21 @@ const formatBudget = budget => {
 export default record => {
   // Preprocess budget
   const budgetObject = {
-    total_cost: formatBudget(record['total project budget']),
-    eu_contrib: formatBudget(record['eu budget contribution']),
+    total_cost: formatBudget(record.Total_project_budget),
+    eu_contrib: formatBudget(record.EU_Budget_contribution),
     private_fund: null,
     public_fund: null,
     other_contrib: null,
-    funding_area: record.funds || null,
+    funding_area: record.Funds || null,
   };
 
   // Preprocess project locations
   const locationArray = [];
-  const countryArray = record['project country']
-    ? record['project country'].split('; ')
+  const countryArray = record.Project_country
+    ? record.Project_country.split('; ')
     : null;
   const previousCountries = [];
-  if (countryArray.length > 1) {
+  if (countryArray !== null && countryArray.length > 1) {
     for (let i = 0; i < countryArray.length; i += 1) {
       if (previousCountries.indexOf(countryArray[i] === -1)) {
         locationArray.push({
@@ -88,9 +88,9 @@ export default record => {
   } else {
     locationArray.push({
       country_name: null,
-      country_code: record['project country'],
-      region: record['project region'],
-      nuts2: record['project nuts2 code'],
+      country_code: record.Project_country,
+      region: record.Project_region,
+      nuts2: record.Project_NUTS2_code,
       location: {
         lat: 0,
         lon: 0,
@@ -99,38 +99,38 @@ export default record => {
   }
 
   // Preprocess type
-  const typeArray = [record['project type']];
+  const typeArray = [record.Project_type];
 
   // Preprocess themes
-  const themeArray = record.themes ? record.themes.split('; ') : null;
+  const themeArray = record.Thèmes ? record.Thèmes.split('; ') : null;
 
   // Preprocess partners
   const partnerArray = [
     {
-      name: record.beneficiary,
+      name: record.Beneficiary,
       type: null,
       address: getAddress(record),
       region: null,
-      country: record['beneficiary country'],
+      country: record.Beneficiary_Country,
       website: null,
     },
   ];
 
   // Map the fields
   return {
-    project_id: record.projectid.toString(),
+    project_id: record.PROJECTID.toString(),
     type: typeArray,
-    period: record.period,
-    publication_date: formatDate(record['date published']),
-    title: record['project name'],
+    period: record.Period,
+    title: record.Project_name,
     project_locations: locationArray,
     themes: themeArray,
     budget: budgetObject,
+    description: record.quote,
     project_website: getProjectWebsite(record),
     partners: partnerArray,
     timeframe: {
-      from: formatDate(record['project timeframe start date']),
-      to: formatDate(record['project timeframe end date']),
+      from: formatDate(record.Project_Timeframe_start_date),
+      to: formatDate(record.Project_Timeframe_end_date),
     },
   };
 };
