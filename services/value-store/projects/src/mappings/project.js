5826992737e9dddd1a2a0c9793d689204720213e
Kalin Chernev <kalin.chernev@gmail.com>
2017-12-08 11:43:33 +0100

    feat(elasticsearch): add mappings

--- services/value-store/projects/src/mappings/project.js
+++ services/value-store/projects/src/mappings/project.js
@@ -1,6 +1,32 @@
 module.exports = {
   project: {
     properties: {
+      budget: {
+        properties: {
+          funding_area: {
+            type: 'keyword',
+          },
+        },
+      },
+      computed_key: {
+        type: 'keyword',
+      },
+      coordinators: {
+        properties: {
+          country: {
+            type: 'keyword',
+          },
+        },
+      },
+      ec_priorities: {
+        type: 'keyword',
+      },
+      producer_id: {
+        type: 'keyword',
+      },
+      programme_name: {
+        type: 'keyword',
+      },
       project_locations: {
         properties: {
           location: {
