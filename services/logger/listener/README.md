76a251bd51ec8ac1c8e59ae50ebf440da8e72d54
Yannick Huard <mail@yannickhuard.com>
2017-12-18 11:26:00 +0100

    Index logs

--- services/logger/listener/README.md
+++ services/logger/listener/README.md
@@ -1,1 +1,3 @@
-# Projects related functionalities
+# Central Logger
+
+Push a SNS message to `${self:provider.stage}-logger-listener-onLogEmitted` and it will be saved into ElasticSearch.
--- services/logger/listener/src/events/onLogEmitted.js
+++ services/logger/listener/src/events/onLogEmitted.js
@@ -1,9 +1,8 @@
-// import AWS from 'aws-sdk'; // eslint-disable-line import/no-extraneous-dependencies
-// import elasticsearch from 'elasticsearch';
-// import connectionClass from 'http-aws-es';
+import elasticsearch from 'elasticsearch';
+import connectionClass from 'http-aws-es';
 
 export const handler = (event, context, callback) => {
-  // const { API, INDEX } = process.env;
+  const { API, INDEX } = process.env;
 
   /*
    * Some checks here before going any further
@@ -23,10 +22,55 @@ export const handler = (event, context, callback) => {
   /*
    * Extract information from the event
    */
+  try {
+    const message = JSON.parse(snsRecord.Sns.Message);
+    console.log(message);
 
-  console.log(snsRecord);
+    // elasticsearch client configuration
+    const options = {
+      host: `https://${API}`,
+      apiVersion: '5.5',
+      connectionClass,
+      index: INDEX,
+    };
 
-  return callback(null, 'ok');
+    const body = {
+      log: {
+        properties: {},
+      },
+    };
+
+    // elasticsearch client instantiation
+    const client = elasticsearch.Client(options);
+
+    return client.indices
+      .exists({ index: INDEX })
+      .then(exists => {
+        if (!exists) {
+          return client.indices.create({ index: INDEX });
+        }
+        return exists;
+      })
+      .then(() => client.indices.getMapping({ index: INDEX, type: 'log' }))
+      .catch(() =>
+        client.indices.putMapping({ index: INDEX, type: 'log', body })
+      )
+      .then(() => {
+        client.index(
+          {
+            index: INDEX,
+            type: 'log',
+            body: message,
+          },
+          err => {
+            if (err) return callback(err);
+            return callback(null, 'ok');
+          }
+        );
+      });
+  } catch (e) {
+    return callback(e.message);
+  }
 };
 
 export default handler;
