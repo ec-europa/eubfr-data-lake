9f60a527de0cc05e70288e979edbd55ee16766bc
Yannick Huard <mail@yannickhuard.com>
2018-01-08 11:54:26 +0100

    fix(domain): get domain dynamically (without export-env) - EUBFR-101

--- services/value-store/projects/src/mappings/project.js
+++ services/value-store/projects/src/mappings/project.js
@@ -1,53 +1,52 @@
-// Specific syntax https://serverless.com/framework/docs/providers/aws/guide/variables/#reference-variables-in-javascript-files
-// eslint-disable-next-line arrow-body-style
-module.exports = () => {
-  return {
-    project: {
-      properties: {
-        budget: {
-          properties: {
-            funding_area: {
-              type: 'keyword',
-            },
+// Specific syntax:
+// https://serverless.com/framework/docs/providers/aws/guide/variables/#reference-variables-in-javascript-files
+
+module.exports = () => ({
+  project: {
+    properties: {
+      budget: {
+        properties: {
+          funding_area: {
+            type: 'keyword',
           },
         },
-        computed_key: {
-          type: 'keyword',
-        },
-        coordinators: {
-          properties: {
-            country: {
-              type: 'keyword',
-            },
+      },
+      computed_key: {
+        type: 'keyword',
+      },
+      coordinators: {
+        properties: {
+          country: {
+            type: 'keyword',
           },
         },
-        ec_priorities: {
-          type: 'keyword',
-        },
-        producer_id: {
-          type: 'keyword',
-        },
-        programme_name: {
-          type: 'keyword',
-        },
-        project_locations: {
-          properties: {
-            location: {
-              type: 'geo_shape',
-            },
+      },
+      ec_priorities: {
+        type: 'keyword',
+      },
+      producer_id: {
+        type: 'keyword',
+      },
+      programme_name: {
+        type: 'keyword',
+      },
+      project_locations: {
+        properties: {
+          location: {
+            type: 'geo_shape',
           },
         },
-        timeframe: {
-          properties: {
-            from: {
-              type: 'date',
-            },
-            to: {
-              type: 'date',
-            },
+      },
+      timeframe: {
+        properties: {
+          from: {
+            type: 'date',
+          },
+          to: {
+            type: 'date',
           },
         },
       },
     },
-  };
-};
+  },
+});
