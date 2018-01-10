281a00137eea569504dcdea9b3895c49cb440e8a
Yannick Huard <mail@yannickhuard.com>
2018-01-10 13:44:07 +0100

    JS classes, because it will make @kalinchernev happy

--- services/logger/listener/src/lib/Logger.js
+++ services/logger/listener/src/lib/Logger.js
@@ -1,45 +1,43 @@
-function Logger({ sns, targetArn, emitter }) {
-  this.sns = sns;
-  this.targetArn = targetArn;
-  this.emitter = emitter;
-}
+class Logger {
+  constructor({ sns, targetArn, emitter }) {
+    this.sns = sns;
+    this.targetArn = targetArn;
+    this.emitter = emitter;
+  }
 
-Logger.prototype.log = function log({
-  level = 'info',
-  type = 'file',
-  message = '',
-}) {
-  return this.sns
-    .publish({
-      Message: JSON.stringify({
-        default: JSON.stringify({
-          emitter: this.emitter,
-          level,
-          type,
-          time: new Date().toISOString(),
-          message,
+  log({ level = 'info', type = 'file', message = '' }) {
+    return this.sns
+      .publish({
+        Message: JSON.stringify({
+          default: JSON.stringify({
+            emitter: this.emitter,
+            level,
+            type,
+            time: new Date().toISOString(),
+            message,
+          }),
         }),
-      }),
-      MessageStructure: 'json',
-      TargetArn: this.targetArn,
-    })
-    .promise();
-};
+        MessageStructure: 'json',
+        TargetArn: this.targetArn,
+      })
+      .promise();
+  }
 
-Logger.prototype.info = function info({ type = 'file', message = '' }) {
-  return this.log({
-    level: 'info',
-    type,
-    message,
-  });
-};
+  info({ type = 'file', message = '' }) {
+    return this.log({
+      level: 'info',
+      type,
+      message,
+    });
+  }
 
-Logger.prototype.error = function info({ type = 'file', message = '' }) {
-  return this.log({
-    level: 'error',
-    type,
-    message,
-  });
-};
+  error({ type = 'file', message = '' }) {
+    return this.log({
+      level: 'error',
+      type,
+      message,
+    });
+  }
+}
 
 export default Logger;
