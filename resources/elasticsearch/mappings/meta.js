035466ef90e38a6f77c68f3971d14783b4422d68
Yannick Huard <mail@yannickhuard.com>
2018-01-12 11:55:26 +0100

    feat(meta-index): replace dynamodb with elasticsearch - EUBFR-104

---
+++ resources/elasticsearch/mappings/meta.js
@@ -0,0 +1,19 @@
+// Specific syntax:
+// https://serverless.com/framework/docs/providers/aws/guide/variables/#reference-variables-in-javascript-files
+
+module.exports = () => ({
+  file: {
+    properties: {
+      emitter: { type: 'keyword' },
+      level: { type: 'keyword' },
+      time: { type: 'date' },
+      message: {
+        type: 'nested',
+        properties: {
+          computed_key: { type: 'keyword' },
+          status_message: { type: 'text' },
+        },
+      },
+    },
+  },
+});
