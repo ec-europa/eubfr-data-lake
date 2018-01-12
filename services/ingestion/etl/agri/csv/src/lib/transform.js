be04b8be309e837635e1e17aa969724a574d9081
Kalin Chernev <kalinchernev@users.noreply.github.com>
2018-01-12 14:44:03 +0100

    refactor(etl): add types to all transform functions - EUBFR-108 (#69)
    * Apply type checking on all ETLs
    
    * Timeframe formatting
    
    * Make project results optional as inforegio xml etl does not have any information about it
    
    * Make project related_links field  optional as inforegio xml etl does not have any information about it
    
    * Location field shape
    
    * Add a required field
    
    * Default parters would be empty array rather than null
    
    * Several fixes
    
    * Fix the rest
    
    * Update babel parser settings for flow
    
    * Date should default to null for elasticsearch
    
    * Bring in fields from budg xls transform
    
    * Add missing fields in other ETLs
    
    * Unifiy all ETLs
    
    * Regenerate yarn.lock
    
    * Update yarn.lock
    
    * Make all fields require

--- services/ingestion/etl/agri/csv/src/lib/transform.js
+++ services/ingestion/etl/agri/csv/src/lib/transform.js
@@ -20,62 +20,6 @@ export default (record: Object): Project => {
     funding_area: '',
   };
 
-  // Preprocess timeframe
-  let timeframeFrom = '';
-  let timeframeTo = '';
-
-  if (record['Timeframe start'].indexOf(' to ') !== -1) {
-    const timeframe = (record['Timeframe start'] || '').split(' to ');
-
-    if (Array.isArray(timeframe) && timeframe.length === 2) {
-      [timeframeFrom, timeframeTo] = timeframe;
-    }
-  } else {
-    timeframeFrom = record['Timeframe start'];
-    timeframeTo = record['Timeframe end'];
-  }
-
-  // Preprocess related links
-  const links = (record['Related links'] || '')
-    .split(';')
-    .map(link => {
-      const matches = link.match(/<a .*href="(.*)".*>(.*)<\/a>/i);
-
-      if (Array.isArray(matches) && matches.length === 3) {
-        return {
-          url: matches[1],
-          label: matches[2],
-        };
-      }
-
-      return {
-        url: '',
-        label: '',
-      };
-    })
-    .filter(link => link !== null);
-
-  // Preprocess project locations
-  const latArray = record['Project location latitude'].split(';');
-  const longArray = record['Project location longitude'].split(';');
-  const locationArray = record['Project country(ies)']
-    .split(';')
-    .map((country, index) => ({
-      country_code: country,
-      region: '',
-      nuts2: '',
-      address: record['Project address(es)'] || '',
-      postal_code: record['Project postal code(s)'] || '',
-      town: record['Project town(s)'] || '',
-      location: {
-        type: 'Point',
-        coordinates: [
-          parseFloat(Array.isArray(longArray) && longArray[index]) || 0,
-          parseFloat(Array.isArray(latArray) && latArray[index]) || 0,
-        ],
-      },
-    }));
-
   // Preprocess coordinators
   const coordArray = record.Coordinators.split(';').map(coordinator => ({
     name: coordinator,
@@ -98,25 +42,88 @@ export default (record: Object): Project => {
     website: '',
   }));
 
+  // Preprocess locations
+  const longArray = record['Project location longitude'].split(';');
+  const latArray = record['Project location latitude'].split(';');
+  const locationArray = record['Project country(ies)']
+    .split(';')
+    .map((country, index) => ({
+      country_code: country,
+      region: '',
+      nuts2: '',
+      address: record['Project address(es)'] || '',
+      postal_code: record['Project postal code(s)'] || '',
+      town: record['Project town(s)'] || '',
+      location: {
+        type: 'Point',
+        coordinates: [
+          parseFloat(Array.isArray(longArray) && longArray[index]) || 0,
+          parseFloat(Array.isArray(latArray) && latArray[index]) || 0,
+        ],
+      },
+    }));
+
+  // Preprocess related links
+  const links = (record['Related links'] || '')
+    .split(';')
+    .map(link => {
+      const matches = link.match(/<a .*href="(.*)".*>(.*)<\/a>/i);
+
+      if (Array.isArray(matches) && matches.length === 3) {
+        return {
+          url: matches[1],
+          label: matches[2],
+        };
+      }
+
+      return {
+        url: '',
+        label: '',
+      };
+    })
+    .filter(link => link !== null);
+
   // Preprocess results
   const resultObject = {
     available: '',
     result: record.Results || '',
   };
 
+  // Preprocess timeframe
+  let timeframeFrom = null;
+  let timeframeTo = null;
+
+  if (record['Timeframe start'].indexOf(' to ') !== -1) {
+    const timeframe = (record['Timeframe start'] || '').split(' to ');
+
+    if (Array.isArray(timeframe) && timeframe.length === 2) {
+      [timeframeFrom, timeframeTo] = timeframe;
+    }
+  } else {
+    timeframeFrom = record['Timeframe start'];
+    timeframeTo = record['Timeframe end'];
+  }
+
   // Map the fields
   return {
-    project_id: record.Nid || '',
-    title: record.Name || '',
-    cover_image: record.Visual || '',
-    programme_name: record['Programme name'] || '',
-    description: record['Project description'] || '',
-    results: resultObject,
-    ec_priorities: record['EC’s priorities'].split(';') || [],
-    coordinators: coordArray,
+    action: '',
     budget: budgetObject,
+    call_year: '',
+    coordinators: coordArray,
+    cover_image: record.Visual || '',
+    description: record['Project description'] || '',
+    ec_priorities: record['EC’s priorities'].split(';') || [],
     partners: partnerArray,
+    programme_name: record['Programme name'] || '',
+    project_id: record.Nid || '',
     project_locations: locationArray,
+    project_website: record['Project webpage'] || '',
+    related_links: links,
+    results: resultObject,
+    status: '',
+    sub_programme_name: '',
+    success_story: '',
+    themes: [],
     timeframe: {
       from:
         timeframeFrom &&
@@ -124,7 +131,7 @@ export default (record: Object): Project => {
       to:
         timeframeTo && new Date(parseInt(timeframeTo, 10) * 1000).toISOString(),
     },
-    project_website: record['Project webpage'] || '',
-    related_links: links,
+    title: record.Name || '',
+    type: [],
   };
 };
--- services/ingestion/etl/types/Project.js
+++ services/ingestion/etl/types/Project.js
@@ -1,5 +1,27 @@
 // @flow
 
+/**
+ *
+ * Project model has been continously discussed in EUBFR-4 EUBFR-5 and EUBFR-70.
+ *
+ * Exported Project interface should ALWAYS have fields as required. When a producer
+ * does not provide data for a field, provide null or '' from transform function.
+ *
+ * Note that this file describes types which are used during static code analysis.
+ * This helps developers write consistend transform functions in ETLs.
+ * This does not mean other layers are safe: elasticsearch, HTTP requests, etc.
+ *
+ * Following is a list of specifics which are here to interoperate with other layers.
+ *
+ * Dates:
+ * Put default `null` when missing data. Elasticsearch will not accept empty string.
+ * More: https://www.elastic.co/guide/en/elasticsearch/reference/6.0/date.html
+ *
+ * Geolocation:
+ * Put default `null` when missing data. Elasticsearch has the notion of geo_shape.
+ * More: https://www.elastic.co/guide/en/elasticsearch/reference/6.0/geo-shape.html
+ */
+
 // eslint-disable-next-line
 import type {
   Point2D,
@@ -10,13 +32,6 @@ import type {
   MultiPolygon2D,
 } from 'flow-geojson';
 
-// Project model is discussed in EUBFR-4 EUBFR-5 and EUBFR-70
-
-type Result = {
-  available?: string,
-  result: string,
-};
-
 type GeoJSON =
   | Point2D
   | MultiPoint2D
@@ -25,48 +40,45 @@ type GeoJSON =
   | Polygon2D
   | MultiPolygon2D;
 
-type Location = {
-  country_code: string,
-  region?: string,
-  nuts2?: string,
-  address?: string,
-  postal_code?: string,
-  town?: string,
-  location: GeoJSON,
-};
-
-type Timeframe = {
-  from: string,
-  to: string,
+type Budget = {
+  eu_contrib: number,
+  total_cost: number,
+  private_fund: number,
+  public_fund: number,
+  other_contrib: number,
+  funding_area: string,
 };
 
 type Coordinator = {
   name: string,
-  type?: string,
-  address?: string,
-  region?: string,
-  country?: string,
-  website?: string,
-  phone?: string,
-  email?: string,
+  type: string,
+  address: string,
+  region: string,
+  country: string,
+  website: string,
+  phone: string,
+  email: string,
+};
+
+type Location = {
+  country_code: string,
+  region: string,
+  nuts2: string,
+  address: string,
+  postal_code: string,
+  town: string,
+  // If nothing else, provide null in transform function for this field.
+  // Never null project_locations field which is typed on ES level.
+  location: GeoJSON | null,
 };
 
 type Partner = {
   name: string,
-  type?: string,
-  address?: string,
-  region?: string,
-  country?: string,
+  type: string,
+  address: string,
+  region: string,
+  country: string,
   website?: string,
-};
-
-type Budget = {
-  eu_contrib: number,
-  total_cost?: number,
-  private_fund?: number,
-  public_fund?: number,
-  other_contrib?: number,
-  funding_area?: string,
 };
 
 type RelatedLink = {
@@ -74,19 +86,36 @@ type RelatedLink = {
   label: string,
 };
 
+type Result = {
+  available: string,
+  result: string,
+};
+
+type Timeframe = {
+  from: string | null,
+  to: string | null,
+};
+
 export type Project = {
-  project_id: string,
-  title: string,
-  description: string,
-  cover_image: string,
-  programme_name: string,
-  project_website: string,
+  action: string,
   budget: Budget,
-  results: Result,
-  timeframe: Timeframe,
-  ec_priorities: Array<string>,
+  call_year: string,
   coordinators: Array<Coordinator>,
+  cover_image: string,
+  description: string,
+  ec_priorities: Array<string>,
   partners: Array<Partner>,
+  programme_name: string,
+  project_id: string,
   project_locations: Array<Location>,
+  project_website: string,
   related_links: Array<RelatedLink>,
+  results: Result,
+  status: string,
+  sub_programme_name: string,
+  success_story: string,
+  themes: Array<string>,
+  timeframe: Timeframe,
+  title: string,
+  type: Array<string>,
 };
