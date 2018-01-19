7e9a8f3cb9b3925ec64d8fccf07da01f1f86ecdd
Kalin Chernev <kalinchernev@users.noreply.github.com>
2018-01-19 11:59:07 +0100

    test(services): improve coverage - EUBFR-117 (#76)
    * Add tests for BUDG/XLS
    
    * Add jest dependency to all packages that will needed it
    
    * Add tests for the rest
    
    * Update file path
    
    * Update tests
    
    * Dates should be predictable in different timezones
    
    * Repeat
    
    * Repeat
    
    * All package.json files to be consistent
    
    * Simplify date formatters
    
    * Correct date format
    
    * Test the tests on drone
    
    * Remove moment deps
    
    * UTC
    
    * tests(sample): show some tests (#77)
    
    * Correct date formatters

---
+++ services/ingestion/etl/inforegio/json/test/unit/lib/transform.spec.js
@@ -0,0 +1,22 @@
+/**
+ * @jest-environment node
+ */
+
+import mapper from '../../../src/lib/transform';
+import testRecord from '../../stubs/record.json';
+
+describe(`DG INFOREGIO JSON transformer`, () => {
+  let result = {};
+
+  beforeAll(() => {
+    result = mapper(testRecord);
+  });
+
+  test(`Throws an error on empty input`, () => {
+    expect(mapper).toThrow();
+  });
+
+  test(`Produces correct JSON output structure`, () => {
+    expect(result).toMatchSnapshot();
+  });
+});
