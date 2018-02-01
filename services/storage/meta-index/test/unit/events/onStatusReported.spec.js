0a22ab4d15157d9f17983a62130e728c6209e6e0
Yannick Huard <mail@yannickhuard.com>
2018-02-01 17:22:20 +0100

    Fix unit tests

--- services/storage/meta-index/test/unit/events/onStatusReported.spec.js
+++ services/storage/meta-index/test/unit/events/onStatusReported.spec.js
@@ -2,18 +2,21 @@
  * @jest-environment node
  */
 
-import { promisify } from 'util';
 import onStatusReported from '../../../src/events/onStatusReported';
 
-const handler = promisify(onStatusReported);
-
 describe(`Function onCreate in "@eubfr/storage-meta-index"`, () => {
-  test('The function expects a correct SNS record', () => {
+  test('The function expects a correct SNS record', async () => {
     const event = {};
     const context = {};
 
     expect.assertions(1);
-    const result = handler(event, context);
-    return expect(result).rejects.toBe('No record');
+
+    const callback = error => {
+      expect(error.message).toEqual('No record');
+    };
+
+    expect.assertions(1);
+
+    await onStatusReported(event, context, callback);
   });
 });
