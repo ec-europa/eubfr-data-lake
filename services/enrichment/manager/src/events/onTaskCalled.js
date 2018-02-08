e459d4b87bb0297b845cd4a2b23f5d0a751b0a3d
Yannick Huard <mail@yannickhuard.com>
2018-02-08 12:24:14 +0100

    feat(enrichment): get data from gisco - EUBFR-113

--- services/enrichment/manager/src/events/onTaskCalled.js
+++ services/enrichment/manager/src/events/onTaskCalled.js
@@ -1,10 +1,41 @@
+const http = require('http');
+
 export const handler = (event, context, callback) => {
   /**
    * Enrichment logic starts here
    */
+  http
+    .get(
+      {
+        hostname: 'europa.eu',
+        port: 80,
+        path: '/webtools/rest/gisco/api?q=berlin',
+        headers: {
+          'User-Agent': 'eubfr',
+        },
+      },
+      res => {
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', chunk => {
+          rawData += chunk;
+        });
 
-  console.log('Task', event);
-  callback(null, event);
+        res.on('end', () => {
+          try {
+            console.log(rawData);
+          } catch (e) {
+            console.error(e.message);
+          }
+        });
+
+        callback(null, event);
+      }
+    )
+    .on('error', e => {
+      console.error(`Got error: ${e.message}`);
+      callback(e);
+    });
 };
 
 export default handler;
