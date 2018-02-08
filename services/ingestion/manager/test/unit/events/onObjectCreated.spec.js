c89cb379168ff0000956cb564d585d942268f3c5
Kalin Chernev <kalin.chernev@gmail.com>
2018-02-08 17:45:27 +0100

    Update test

--- services/ingestion/manager/test/unit/events/onObjectCreated.spec.js
+++ services/ingestion/manager/test/unit/events/onObjectCreated.spec.js
@@ -7,7 +7,7 @@ import onObjectCreated from '../../../src/events/onObjectCreated';
 describe(`Function onCreate in "@eubfr/ingestion-manager"`, () => {
   test('The function expects API, INDEX, REGION and STAGE environment variables', async () => {
     const event = {};
-    const context = {};
+    const context = { awsRequestId: 'foo' };
     const callback = error => {
       expect(error.message).toEqual(
         'API, INDEX, REGION and STAGE environment variables are required!'
