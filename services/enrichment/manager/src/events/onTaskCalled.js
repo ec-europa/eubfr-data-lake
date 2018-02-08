db1a1fd7fc09fb4ff2b3ae914fa4e6b0dbb33094
Yannick Huard <mail@yannickhuard.com>
2018-02-08 16:59:25 +0100

    Start structuring the enricher

--- services/enrichment/manager/src/events/onTaskCalled.js
+++ services/enrichment/manager/src/events/onTaskCalled.js
@@ -1,41 +1,76 @@
-const http = require('http');
+const countries = require('i18n-iso-countries');
+const request = require('request-promise-native');
 
-export const handler = (event, context, callback) => {
-  /**
-   * Enrichment logic starts here
-   */
-  http
-    .get(
-      {
-        hostname: 'europa.eu',
-        port: 80,
-        path: '/webtools/rest/gisco/api?q=berlin',
-        headers: {
-          'User-Agent': 'eubfr',
-        },
-      },
-      res => {
-        res.setEncoding('utf8');
-        let rawData = '';
-        res.on('data', chunk => {
-          rawData += chunk;
+export const handler = async (event, context, callback) => {
+  const record = JSON.parse(event.Body);
+
+  let enriched = false;
+
+  if (!record || !record.project_locations) {
+    return callback(null, 'nothing to do');
+  }
+
+  record.project_locations.forEach(async loc => {
+    if (!loc) {
+      return null;
+    }
+
+    // Country code is provided but location is empty
+    if (loc.country_code && !loc.location) {
+      console.log('Will infer location from country_code: ', loc.country_code);
+
+      // Get country name from ISO 3166-1 Alpha-2 code (doesn't seem mandatory after some testing...)
+      const country = countries.getName(loc.country_code, 'en');
+
+      const url = `http://europa.eu/webtools/rest/gisco/nominatim/search.php?format=json&country=${encodeURIComponent(
+        country
+      )}`;
+
+      try {
+        const results = await request.get({
+          url,
+          json: true,
+          headers: {
+            'User-Agent': 'eubfr',
+          },
         });
 
-        res.on('end', () => {
-          try {
-            console.log(rawData);
-          } catch (e) {
-            console.error(e.message);
-          }
-        });
-
-        callback(null, event);
+        enriched = true;
+        console.log('results', results);
+        if (results && results[0]) {
+          /*
+          lat: '42.6073975',
+          lon: '25.4856617',
+          */
+          console.log('coordinates of ', country, ' are ', [
+            results[0].lon,
+            results[0].lat,
+          ]);
+        }
+      } catch (e) {
+        console.error(e);
       }
-    )
-    .on('error', e => {
-      console.error(`Got error: ${e.message}`);
-      callback(e);
-    });
+
+      return null;
+    } else if (
+      !loc.country_code &&
+      loc.location &&
+      loc.location.type === 'Point' &&
+      loc.location.coordinates
+    ) {
+      console.log(
+        'Will infer country_code from location',
+        loc.location.coordinates
+      );
+      enriched = true;
+      return null;
+    }
+
+    return null;
+  });
+
+  console.log('enriched?', enriched);
+  return null; // callback(null, event);
 };
 
 export default handler;
