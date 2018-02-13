e7627dba7ad00b8e2485152982061ab9c44ddc74
Kalin Chernev <kalin.chernev@gmail.com>
2018-02-13 10:09:49 +0100

    Fix deletion scripts

--- scripts/delete-demo.js
+++ scripts/delete-demo.js
@@ -17,12 +17,17 @@ if (process.env.EUBFR_USERNAME) {
 
 // Remove dashboards
 usernames.forEach(username => {
+  // Delete S3 buckets holding static assets (the React apps)
   deleteServerlessService('demo-dashboard-client', {
     isClient: true,
     username,
   });
+  // Delete also the CloudFormation stacks created by SLS
+  deleteServerlessService('demo-dashboard-client', { username });
+  // Lastly, delete their back-ends
   deleteServerlessService('demo-dashboard-server', { username });
-});
 
-// Remove website
-deleteServerlessService('demo-website', { isClient: true });
+  // Remove website
+  deleteServerlessService('demo-website', { isClient: true, username });
+  deleteServerlessService('demo-website', { username });
+});
