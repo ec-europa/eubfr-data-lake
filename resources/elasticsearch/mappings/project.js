4fcdd194a7e4a155db8d81642594525bee68792c
Yannick Huard <mail@yannickhuard.com>
2018-02-14 12:24:36 +0100

    fix(es-project-types): add more types to ES "project" index - EUBFR-140

--- resources/elasticsearch/mappings/project.js
+++ resources/elasticsearch/mappings/project.js
@@ -1,83 +1,111 @@
 // Specific syntax:
 // https://serverless.com/framework/docs/providers/aws/guide/variables/#reference-variables-in-javascript-files
 
+const budgetItem = {
+  properties: {
+    currency: { type: 'keyword' },
+    raw: { type: 'text' },
+    value: { type: 'long' },
+  },
+};
+
+const textWithKeyword = {
+  type: 'text',
+  fields: { keyword: { type: 'keyword', ignore_above: 256 } },
+};
+
 module.exports = () => ({
   mappings: {
     project: {
       properties: {
-        last_modified: { type: 'date' },
-        title: {
-          type: 'text',
-          fields: {
-            keyword: {
-              type: 'keyword',
-            },
-          },
-        },
+        action: { type: 'text' },
         budget: {
           properties: {
-            funding_area: {
-              type: 'text',
-              fields: {
-                keyword: {
-                  type: 'keyword',
-                },
-              },
-            },
+            eu_contrib: budgetItem,
+            funding_area: textWithKeyword,
+            mmf_heading: textWithKeyword,
+            other_contrib: budgetItem,
+            private_fund: budgetItem,
+            public_fund: budgetItem,
+            total_cost: budgetItem,
           },
         },
-        computed_key: {
-          type: 'keyword',
-        },
+        call_year: { type: 'text' },
+        computed_key: { type: 'keyword' },
         coordinators: {
+          type: 'nested',
           properties: {
-            country: {
-              type: 'keyword',
-            },
+            address: { type: 'text' },
+            country: { type: 'keyword' },
+            email: { type: 'keyword' },
+            name: { type: 'keyword' },
+            phone: { type: 'keyword' },
+            region: { type: 'keyword' },
+            type: { type: 'keyword' },
+            website: { type: 'keyword' },
           },
         },
-        ec_priorities: {
-          type: 'text',
-          fields: {
-            keyword: {
-              type: 'keyword',
-            },
+        created_by: { type: 'keyword' },
+        description: { type: 'text' },
+        ec_priorities: textWithKeyword,
+        last_modified: { type: 'date' },
+        media: {
+          properties: {
+            cover_image: { type: 'text' },
+            video: { type: 'text' },
           },
         },
-        producer_id: {
-          type: 'keyword',
-        },
-        project_id: {
-          type: 'keyword',
-        },
-        programme_name: {
-          type: 'text',
-          fields: {
-            keyword: {
-              type: 'keyword',
-            },
+        partners: {
+          type: 'nested',
+          properties: {
+            address: { type: 'text' },
+            country: { type: 'keyword' },
+            name: { type: 'keyword' },
+            region: { type: 'keyword' },
+            type: { type: 'keyword' },
+            website: { type: 'keyword' },
           },
         },
+        producer_id: { type: 'keyword' },
+        programme_name: textWithKeyword,
+        project_id: { type: 'keyword' },
         project_locations: {
+          type: 'nested',
           properties: {
-            centroid: {
-              type: 'geo_point',
-            },
-            location: {
-              type: 'geo_shape',
-            },
+            address: { type: 'text' },
+            centroid: { type: 'geo_point' },
+            country_code: { type: 'keyword' },
+            location: { type: 'geo_shape' },
+            nuts2: { type: 'keyword' },
+            postal_code: { type: 'keyword' },
+            region: { type: 'keyword' },
+            town: { type: 'keyword' },
           },
         },
+        project_website: { type: 'text' },
+        related_links: {
+          properties: {
+            label: { type: 'text' },
+            url: { type: 'text' },
+          },
+        },
+        results: {
+          properties: {
+            available: { type: 'keyowrd' },
+            result: { type: 'text' },
+          },
+        },
+        status: { type: 'text' },
+        sub_programme_name: { type: 'text' },
+        success_story: { type: 'text' },
         timeframe: {
           properties: {
-            from: {
-              type: 'date',
-            },
-            to: {
-              type: 'date',
-            },
+            from: { type: 'date' },
+            to: { type: 'date' },
           },
         },
+        title: textWithKeyword,
+        type: { type: 'text' },
       },
     },
   },
