d96e96d65c0c639d2c78859ce421fdf5964942f5
Kalin Chernev <kalin.chernev@gmail.com>
2018-04-09 08:29:37 +0200

    Add more details

--- services/ingestion/helpers/getCountryCode.js
+++ services/ingestion/helpers/getCountryCode.js
@@ -1,4 +1,4 @@
-// Helper to get country code based on few exceptions
+// Helper to translate few specific NUTS0 codes to ISO 3166-1 Alpha-2 country codes
 module.exports = countryCode => {
   switch (countryCode.trim().toUpperCase()) {
     case 'UK':
