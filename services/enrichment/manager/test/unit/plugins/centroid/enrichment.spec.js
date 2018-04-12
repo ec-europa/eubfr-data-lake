d1d38e2481c22dfb1484412131feccffbd1d551b
Kalin Chernev <kalin.chernev@gmail.com>
2018-04-12 15:40:27 +0200

    Rework

--- services/enrichment/manager/test/unit/plugins/centroid/enrichment.spec.js
+++ services/enrichment/manager/test/unit/plugins/centroid/enrichment.spec.js
@@ -5,28 +5,24 @@
 import plugin from '../../../../src/plugins/centroid';
 import location from './location.json';
 
-describe('Centroid enrichment plugin: empty object', () => {
-  const loc = {};
-  let result = {};
-
-  beforeAll(async () => {
-    result = await plugin(loc);
-  });
-
-  test('Returns empty object, untouched, no errors', () => {
+describe('Centroid enrichment plugin: depending on the following input', () => {
+  test('Empty object: returns same object, untouched, no errors', async () => {
+    const loc = {};
+    const result = await plugin(loc);
     expect(result).toMatchSnapshot();
   });
-});
 
-describe('Centroid enrichment plugin: actual input', () => {
-  const loc = location;
-  let result = {};
-
-  beforeAll(async () => {
-    result = await plugin(loc);
+  test('Object with a centroid property containing latidude and logitude: returns a new object with NUTS data', async () => {
+    const loc = JSON.parse(JSON.stringify(location));
+    const result = await plugin(loc);
+    expect(result).toMatchSnapshot();
   });
 
-  test('Returns a new object with NUTs code', () => {
+  test('Object with centroid property containing latidude and logitude, but no country_code, take country_code from NUTS', async () => {
+    const loc = Object.assign({}, location, {
+      country_code: '',
+    });
+    const result = await plugin(loc);
     expect(result).toMatchSnapshot();
   });
 });
