9f4c055018648d1a8c3bd89b016606b6f78cc214
Kalin Chernev <kalin.chernev@gmail.com>
2018-04-30 13:11:48 +0200

    fix(dashboards): not really an inpage nav - EUBFR-171

--- demo/dashboard/client/src/components/SideNav.js
+++ demo/dashboard/client/src/components/SideNav.js
@@ -2,32 +2,15 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const SideNav = () => (
-  <nav className="ecl-navigation-inpage">
-    <div className="ecl-navigation-inpage__body">
-      <ul
-        className="ecl-navigation-inpage__list"
-        aria-labelledby="ecl-navigation-inpage-trigger"
-      >
-        <li className="ecl-navigation-inpage__item">
-          <NavLink
-            to="/files"
-            className="ecl-navigation-inpage__link ecl-link ecl-link--standalone"
-            activeClassName="ecl-navigation-inpage__link--is-active"
-          >
-            My Files
-          </NavLink>
-        </li>
-        <li className="ecl-navigation-inpage__item">
-          <NavLink
-            to="/upload"
-            className="ecl-navigation-inpage__link ecl-link ecl-link--standalone"
-            activeClassName="ecl-navigation-inpage__link--is-active"
-          >
-            Upload a new file
-          </NavLink>
-        </li>
-      </ul>
-    </div>
+  <nav>
+    <ul>
+      <li>
+        <NavLink to="/files">My Files</NavLink>
+      </li>
+      <li>
+        <NavLink to="/upload">Upload a new file</NavLink>
+      </li>
+    </ul>
   </nav>
 );
 
