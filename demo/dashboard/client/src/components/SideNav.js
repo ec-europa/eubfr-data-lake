49cc5d51a30b9fc97fe15ab618e5212a088b63a6
Radu Chiriac <contact@raduchiriac.me>
2018-04-30 16:34:23 +0200

    Fix inpage-navigation active class

--- demo/dashboard/client/src/components/SideNav.js
+++ demo/dashboard/client/src/components/SideNav.js
@@ -2,15 +2,37 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const SideNav = () => (
-  <nav>
-    <ul>
-      <li>
-        <NavLink to="/files">My Files</NavLink>
-      </li>
-      <li>
-        <NavLink to="/upload">Upload a new file</NavLink>
-      </li>
-    </ul>
+  <nav
+    className="ecl-inpage-navigation ecl-u-align-self-start ecl-inpage-navigation--sticky ecl-inpage-navigation--changed"
+    style={{ top: '0px', position: 'sticky' }}
+  >
+    <div className="ecl-inpage-navigation__body">
+      <ul
+        className="ecl-inpage-navigation__list"
+        aria-hidden="true"
+        aria-labelledby="ecl-inpage-navigation-trigger"
+        id="ecl-inpage-navigation-list"
+      >
+        <li className="ecl-inpage-navigation__item">
+          <NavLink
+            className="ecl-link ecl-link--standalone ecl-inpage-navigation__link"
+            activeClassName="ecl-inpage-navigation__link--is-active"
+            to="/files"
+          >
+            My Files
+          </NavLink>
+        </li>
+        <li className="ecl-inpage-navigation__item">
+          <NavLink
+            className="ecl-link ecl-link--standalone ecl-inpage-navigation__link"
+            activeClassName="ecl-inpage-navigation__link--is-active"
+            to="/upload"
+          >
+            Upload a new file
+          </NavLink>
+        </li>
+      </ul>
+    </div>
   </nav>
 );
 
