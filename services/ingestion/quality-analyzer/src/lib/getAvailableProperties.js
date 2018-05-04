c71b3c50bb63084f7d06a1f76cfff6660132c9e7
Kalin Chernev <kalin.chernev@gmail.com>
2018-05-04 14:39:23 +0200

    Handle some special cases

--- services/ingestion/quality-analyzer/src/lib/getAvailableProperties.js
+++ services/ingestion/quality-analyzer/src/lib/getAvailableProperties.js
@@ -1,3 +1,35 @@
+const specialFields = [
+  'total_cost',
+  'eu_contrib',
+  'private_fund',
+  'public_fund',
+  'other_contrib',
+  'centroid',
+  'location',
+];
+
+const getNormalizedObject = initialObject => {
+  const has = (object, key) =>
+    object ? hasOwnProperty.call(object, key) : false;
+
+  // A monetary field in budget, value is enough of the all
+  if (has(initialObject, 'currency')) {
+    return { value: initialObject.value };
+  }
+
+  // A centroid with latitude and longitude
+  if (has(initialObject, 'lat') || has(initialObject, 'lon')) {
+    return { skipDepper: true };
+  }
+
+  // We care only if there are coordinates or not, not about their values
+  if (has(initialObject, 'type') && has(initialObject, 'coordinates')) {
+    return { coordinates: true };
+  }
+
+  return initialObject;
+};
+
 const hasUsefulData = input => {
   const unAcceptableValues = [0, '0', 'EUR', '', null];
 
@@ -14,12 +46,16 @@ const getAvailableProperties = (obj, results, stack = '') => {
   }
 
   Object.keys(obj).forEach(property => {
-    // If the value of this property is an object, then it's nested, re-iterate
     if (typeof obj[property] === 'object') {
+      const fieldIsSpecial = specialFields.includes(property);
+      const ObjectUnderInvestigation = fieldIsSpecial
+        ? getNormalizedObject(obj[property])
+        : obj[property];
+
       // If no previous stack, then property is sufficient as a root.
       const setStack = stack === '' ? property : `${stack}.${property}`;
       // Re-iterate
-      getAvailableProperties(obj[property], results, setStack);
+      getAvailableProperties(ObjectUnderInvestigation, results, setStack);
     } else if (hasUsefulData(obj[property]) && !results[stack]) {
       // nothing nested, use the property
       if (stack === '') {
