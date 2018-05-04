6e4a346f6018ee41d542cb63e9ebcf22892d6321
Kalin Chernev <kalin.chernev@gmail.com>
2018-05-04 10:17:19 +0200

    Add the rest of the etls

--- services/ingestion/quality-analyzer/test/unit/lib/getAvailableProperties.spec.js
+++ services/ingestion/quality-analyzer/test/unit/lib/getAvailableProperties.spec.js
@@ -7,25 +7,22 @@ import getAvailableProperties from '../../../src/lib/getAvailableProperties';
 
 describe(`Helper getAvailableProperties "@eubfr/ingestion-quality-analyzer" works for any existing transform`, () => {
   const transforms = [
-    'agri-csv',
-    // 'budg-xls',
-    // 'iati-csv',
-    // 'inforegio-json',
-    // 'inforegio-xml',
-    // 'valor-xls',
-    // 'wifi4eu-xls',
+    'agri/csv',
+    'budg/xls',
+    'iati/csv',
+    'inforegio/json',
+    'inforegio/xml',
+    'valor/xls',
+    'wifi4eu/xls',
   ];
 
   transforms.forEach(transform => {
     test(`Works with results of ${transform}`, () => {
       const results = [];
-      const info = transform.split('-');
-      const producer = info[0];
-      const extension = info[1];
 
       // eslint-disable-next-line
       const record = require(path.resolve(
-        `${__dirname}/../../stubs/${producer}/${extension}/record.json`
+        `${__dirname}/../../stubs/${transform}/record.json`
       ));
 
       getAvailableProperties(record, results);
