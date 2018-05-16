f48fcf430011ec2caea1af074c6a964d2ac65611
Kalin Chernev <kalin.chernev@gmail.com>
2018-05-16 18:01:16 +0200

    Corrections

--- services/value-store/projects-kinesis/webpack.config.js
+++ services/value-store/projects-kinesis/webpack.config.js
@@ -4,28 +4,19 @@ const path = require('path');
 module.exports = {
   entry: slsw.lib.entries,
   target: 'node',
-  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
-  optimization: {
-    minimize: process.env.EUBFR_ENV && process.env.EUBFR_ENV === 'prod',
-  },
-  devtool: 'nosources-source-map',
   externals: [{ 'aws-sdk': true }],
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: [
-          {
-            loader: 'babel-loader',
-          },
-        ],
+        use: ['babel-loader'],
         include: __dirname,
         exclude: /node_modules/,
       },
     ],
   },
   output: {
-    libraryTarget: 'commonjs2',
+    libraryTarget: 'commonjs',
     path: path.join(__dirname, '.webpack'),
     filename: '[name].js',
   },
