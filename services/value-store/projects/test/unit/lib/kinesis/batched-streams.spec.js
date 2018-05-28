eb52f036b617486b387cf891c885d1126f752df7
Kalin Chernev <kalin.chernev@gmail.com>
2018-05-28 11:54:37 +0200

    Add simple test for making sure through2-batch goes through all records

---
+++ services/value-store/projects/test/unit/lib/kinesis/batched-streams.spec.js
@@ -0,0 +1,48 @@
+/**
+ * @jest-environment node
+ */
+
+import fs from 'fs';
+import path from 'path';
+import split2 from 'split2';
+import through2Batch from 'through2-batch';
+
+describe('Using through2-batch', () => {
+  test('Ensures all records are batched correctly', () => {
+    const file = path.resolve(`${__dirname}/../../stubs/sample.ndjson`);
+    const fileReadStream = fs.createReadStream(file);
+
+    let batches = 0;
+    let records = 0;
+
+    return new Promise((resolve, reject) => {
+      fileReadStream
+        .on('error', e => reject(e))
+        .pipe(split2(JSON.parse))
+        .on('error', e => reject(e))
+        .pipe(
+          through2Batch.obj({ batchSize: 10 }, (batch, _, cb) => {
+            // count the batch
+            batches += 1;
+
+            const improvedBatch = batch.map(item => {
+              // count the records
+              records += 1;
+
+              // Emulate some sort of transformation in batch
+              return Object.assign({ foo: 'bar' }, item);
+            });
+
+            return cb(null, improvedBatch);
+          })
+        )
+        .on('error', e => reject(e))
+        .on('finish', () => {
+          // Basically, make sure all records are taken into account.
+          expect(batches).toEqual(2);
+          expect(records).toEqual(15);
+          resolve();
+        });
+    });
+  });
+});
