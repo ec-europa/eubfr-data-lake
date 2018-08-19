8dc035b1674eba2a6e42825bf16991dfbaaa9b8a
Kalin Chernev <kalin.chernev@gmail.com>
2018-08-19 13:12:16 +0300

    Working example snapshot

--- scripts/documentation/docs-md.js
+++ scripts/documentation/docs-md.js
@@ -4,38 +4,38 @@ const fs = require('fs');
 const path = require('path');
 const documentation = require('documentation'); // eslint-disable-line import/no-extraneous-dependencies
 
-// documentation
-//   .build('types/*.js', {})
-//   .then(documentation.formats.md)
-//   .then(output => {
-//     fs.writeFileSync(path.resolve('./docs/types/Project.md'), output);
-//   });
+documentation
+  .build('types/*.js', {})
+  .then(documentation.formats.md)
+  .then(output => {
+    fs.writeFileSync(path.resolve('./docs/types/Project.md'), output);
+  });
 
-// const transforms = [
-//   'agri-csv',
-//   'budg-xls',
-//   'cordis-csv',
-//   'home-xls',
-//   'iati-csv',
-//   'inforegio-json',
-//   'inforegio-xml',
-//   'valor-xls',
-//   'wifi4eu-xls',
-// ];
+const transforms = [
+  'agri-csv',
+  'budg-xls',
+  'cordis-csv',
+  'home-xls',
+  'iati-csv',
+  'inforegio-json',
+  'inforegio-xml',
+  'valor-xls',
+  'wifi4eu-xls',
+];
 
-// transforms.forEach(transform => {
-//   const etl = transform.split('-');
+transforms.forEach(transform => {
+  const etl = transform.split('-');
 
-//   documentation
-//     .build(`**/etl/${etl[0]}/${etl[1]}/**/transform.js`, {})
-//     .then(documentation.formats.md)
-//     .then(output => {
-//       fs.writeFileSync(
-//         path.resolve(`./docs/types/etls/${transform}.md`),
-//         output
-//       );
-//     });
-// });
+  documentation
+    .build(`**/etl/${etl[0]}/${etl[1]}/**/transform.js`, {})
+    .then(documentation.formats.md)
+    .then(output => {
+      fs.writeFileSync(
+        path.resolve(`./docs/types/etls/${transform}.md`),
+        output
+      );
+    });
+});
 
 // CLI documentation is inside the commands, which are basically placeholders.
 documentation
--- tools/eubfr-cli/commands/es/backupKibana.js
+++ tools/eubfr-cli/commands/es/backupKibana.js
@@ -9,8 +9,18 @@ const getUserCredentials = promisify(awscred.load);
 
 /**
  * Backs up the `.kibana` index of a given ES domain host path.
+ * @see https://aws.amazon.com/blogs/database/use-amazon-s3-to-store-a-single-amazon-elasticsearch-service-index/
+ * Policy and role with name `eubfr-es-repository` have already been created in the EUBFR AWS account, no need to do that through sls and CF.
+ * And the connection between `eubfr-es-repository` policy doc and `eubfr-es-repository` role has been already made.
+ * You now use the role that sets up a trust relationship between Amazon ES and Amazon S3, allowing Amazon ES to write to S3 bucket.
  */
 const backupKibana = async ({ host }) => {
+  // For the moment, same name, but keeping it flexible.
+  // Created by resources/backup-storage
+  const bucket = 'eubfr-es-repository';
+  // Created already once in the AWS console
+  const role = 'eubfr-es-repository';
+
   try {
     // Get user's AWS credentials and region
     const credentials = await getUserCredentials();
@@ -31,24 +41,28 @@ const backupKibana = async ({ host }) => {
     };
 
     const client = elasticsearch.Client(esOptions);
+    // Get information about the ES role with access to S3
+    const iam = new AWS.IAM();
+    const roleDetalis = await iam.getRole({ RoleName: role }).promise();
 
     // Try to get information about an existing repository
     try {
+      console.log('Check for existing repository for Kibana ...');
       const check = await client.snapshot.getRepository({
         repository: 'kibana',
       });
       console.log(check);
     } catch (error) {
-      // If not found, create one.
       if (error.displayName === 'NotFound') {
+        console.log('Kibana repository not found, creting one ...');
         const repo = await client.snapshot.createRepository({
           repository: 'kibana',
           body: {
             type: 's3',
             settings: {
-              bucket: 'eubfr-backup',
+              bucket,
               endpoint: region,
-              role_arn: '',
+              role_arn: roleDetalis.Role.Arn,
             },
           },
         });
@@ -56,16 +70,18 @@ const backupKibana = async ({ host }) => {
       }
     }
 
+    console.log('Creating a snapshot for Kibana index ...');
+
     const info = await client.snapshot.create({
       repository: 'kibana',
       snapshot: 'dev',
       body: {
         type: 's3',
         settings: {
-          bucket: 'eubfr-backup',
+          bucket,
           compress: true,
           region,
-          role_arn: '',
+          role_arn: roleDetalis.Role.Arn,
         },
       },
     });
