979731b3cafaa93b6f92aaa38ed1b3ea090c2c32
Kalin Chernev <kalin.chernev@gmail.com>
2018-08-29 12:09:14 +0300

    First working ecs.runTask() example

---
+++ services/ingestion/etl/iati/csv/src/events/onParseCSVDlq.js
@@ -0,0 +1,72 @@
+import AWS from 'aws-sdk'; // eslint-disable-line import/no-extraneous-dependencies
+
+import MessengerFactory from '@eubfr/logger-messenger/src/lib/MessengerFactory';
+import { STATUS } from '@eubfr/logger-messenger/src/lib/status';
+
+import { extractMessage } from '../lib/sns';
+
+export const handler = async (event, context, callback) => {
+  const ecs = new AWS.ECS();
+  const messenger = MessengerFactory.Create({ context });
+
+  // Extract message
+  const sqsRecord = event.Records ? event.Records[0] : undefined;
+  const initialMessage = JSON.parse(sqsRecord.body);
+  const message = extractMessage(initialMessage);
+  const { key } = message.object;
+
+  const handleError = async e => {
+    await messenger.send({
+      message: {
+        computed_key: key,
+        status_message: e.message,
+        status_code: STATUS.ERROR,
+      },
+      to: ['logs'],
+    });
+  };
+
+  try {
+    await messenger.send({
+      message: {
+        computed_key: key,
+        status_message:
+          'Regular ETL handler failed. Starting a separate long-running task.',
+        status_code: STATUS.ERROR,
+      },
+      to: ['logs'],
+    });
+
+    const runParams = {
+      taskDefinition: 'runner1:1',
+      launchType: 'FARGATE',
+      networkConfiguration: {
+        awsvpcConfiguration: {
+          assignPublicIp: 'ENABLED',
+          subnets: ['subnet-ea11e581'],
+        },
+      },
+      overrides: {
+        containerOverrides: [
+          {
+            environment: [
+              {
+                name: 'AWS_LAMBDA_FUNCTION_EVENT',
+                value: JSON.stringify(initialMessage),
+              },
+            ],
+            name: 'runner1',
+          },
+        ],
+      },
+    };
+
+    const result = await ecs.runTask(runParams).promise();
+    return callback(null, result);
+  } catch (e) {
+    await handleError(e);
+    return callback(e);
+  }
+};
+
+export default handler;
