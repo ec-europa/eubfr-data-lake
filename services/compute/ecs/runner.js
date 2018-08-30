1db6202424b6be83eb3afafe1935d717b14eef3a
Kalin Chernev <kalin.chernev@gmail.com>
2018-08-30 14:10:05 +0300

    Save

--- services/compute/ecs/runner.js
+++ services/compute/ecs/runner.js
@@ -1,16 +1,63 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
+
+const AWS = require('aws-sdk');
 const { argv } = require('yargs');
+const unzip = require('unzip');
 
-// Accept dynamic parameters passed as environment variables from the container.
-console.log(argv);
+// const awscred = require('awscred');
+// const getUserCredentials = promisify(awscred.load);
 
-// runner accepts (event, context) from handler invoking the ECS task
+// Runner definition
+const runner = async () => {
+  // Get original event and context.
+  console.log(JSON.stringify(argv));
+  const { event, context: c } = argv;
+  const context = JSON.parse(c);
 
-// Use context.invokedFunctionArn
+  if (!context.functionName) {
+    return console.error('Required function name not found in context.');
+  }
+  const invoking = context.functionName;
+  const originalInvoking = invoking.substring(0, invoking.indexOf('Dlq'));
 
-// Use https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Lambda.html#getFunction-property
-// Get the bundled function and build the information about the lambda handler.
+  try {
+    const lambda = new AWS.Lambda({ region: 'eu-central-1' });
 
-// Invoke the built/bundled handler with the original (event, context) inside the container which does not timeout
-// Permissions are already attached on arn:aws:iam::491621799026:role/ecsTaskExecutionRole which will be used by the container's code.
+    const response = await lambda
+      .getFunction({ FunctionName: originalInvoking })
+      .promise();
+
+    const sourceCodeSignedUrl = response.Code.Location;
+
+    return new Promise((resolve, reject) => {
+      https
+        .get(sourceCodeSignedUrl, res => {
+          res.pipe(fs.createWriteStream('./handler.zip'));
+          res.on('end', () => {
+            const source = path.resolve(`${__dirname}/handler.zip`);
+            const sourceZip = fs.createReadStream(source);
+            sourceZip.pipe(unzip.Extract({ path: __dirname }));
+            sourceZip.on('end', async () => {
+              const pathToHandler = path.resolve(
+                `${__dirname}/src/events/onParseCSV.js`
+              );
+              const handler = require(pathToHandler);
+              const result = await handler.handler(event, context);
+              console.log(result);
+              resolve();
+            });
+          });
+        })
+        .on('error', reject);
+    });
+  } catch (e) {
+    return console.error(e.message);
+  }
+};
+
+// Execute the runner.
+runner();
