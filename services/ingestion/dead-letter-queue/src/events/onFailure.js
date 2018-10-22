c9fd76a999c6515e72a11fa54d75e0e11166b0e4
Kalin Chernev <kalin.chernev@gmail.com>
2018-10-22 12:09:43 +0300

    Handle structure of harmonized storage event

--- services/ingestion/dead-letter-queue/src/events/onFailure.js
+++ services/ingestion/dead-letter-queue/src/events/onFailure.js
@@ -3,21 +3,23 @@ import AWS from 'aws-sdk'; // eslint-disable-line import/no-extraneous-dependenc
 import MessengerFactory from '@eubfr/logger-messenger/src/lib/MessengerFactory';
 import { STATUS } from '@eubfr/logger-messenger/src/lib/status';
 
-import { extractMessage, extractTopic } from '../lib/sns';
+import { extractMessage, extractTopic, extractKey } from '../lib/extractors';
 import getHandlerData from '../lib/getHandlerData';
 
 export const handler = async (event, context, callback) => {
   const { RUNNER, BUCKET, REGION, STAGE, SUBNET } = process.env;
+
   const ecs = new AWS.ECS();
   const messenger = MessengerFactory.Create({ context });
 
   // Extract message
   const sqsRecord = event.Records ? event.Records[0] : undefined;
-  const initialMessage = JSON.parse(sqsRecord.body);
 
+  const initialMessage = JSON.parse(sqsRecord.body);
   const message = extractMessage(initialMessage);
   const topicArn = extractTopic(initialMessage);
-  const { key } = message.object;
+  const key = extractKey(message);
+
   const handlerData = getHandlerData(topicArn);
 
   try {
--- services/ingestion/dead-letter-queue/src/lib/extractors.js
+++ services/ingestion/dead-letter-queue/src/lib/extractors.js
@@ -21,3 +21,20 @@ export const extractTopic = event => {
   const topic = snsRecord.Sns.TopicArn;
   return topic;
 };
+
+export const extractKey = message => {
+  // Value store / harmonized storage event message structure.
+  if (
+    message.Records &&
+    message.Records[0] &&
+    message.Records[0].s3 &&
+    message.Records[0].s3.object &&
+    message.Records[0].s3.object.key
+  ) {
+    return message.Records[0].s3.object.key.replace('.ndjson', '');
+  }
+
+  // Regular ETL
+  const { key } = message.object;
+  return key;
+};
