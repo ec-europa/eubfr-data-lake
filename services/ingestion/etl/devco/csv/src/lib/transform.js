bb0f7cf6bda894d8489fb84ed9aedc1cddc48599
Kalin Chernev <kalin.chernev@gmail.com>
2018-10-25 17:00:30 +0300

    Add results

--- services/ingestion/etl/devco/csv/src/lib/transform.js
+++ services/ingestion/etl/devco/csv/src/lib/transform.js
@@ -110,12 +110,27 @@ const getLocations = record => {
  * Preprocess `results`
  *
  * Input fields taken from the `record` are:
- * - `Country`
- * - `Region`
+ *
+ * - "1.1 Access on grid electricity ('000 people)"
+ * - "1.2 Access mini grid electricity ('000 people)"
+ * - "1.3 Access off-grid electricity ('000 people)"
+ * - "1.4 Inferred access (additional generation) ('000 people)"
+ * - "1.5 Inferred access (cross-border transmission) ('000 people)"
+ * - "1.6 Access to biomass/biogas clean cooking ('000 people)"
+ * - "1.7 Access to LPG/ethanol cooking ('000 people)"
+ * - '1.8 Electricity from renewables (GWh/year)'
+ * - '1.9 Renewable generation capacity (MW)'
+ * - '1.10 Electricity from energy efficiency (liberated capacity) (MW)'
+ * - '1.11 Transmission lines (km)'
+ * - '1.12 Distribution lines (km)'
+ * - '1.13 Energy Savings (MWh/year)'
+ * - '1.14 GHG emissions avoided per year (ktons CO2eq)'
+ * - '1.15 No of direct jobs person/year (construction)'
+ * - '1.16 No of permanent jobs \n(operation)'
  *
  * @memberof DevcoCsvTransform
  * @param {Object} record The row received from parsed file
- * @returns {Array}
+ * @returns {Result}
  */
 
 const getResults = record => {
