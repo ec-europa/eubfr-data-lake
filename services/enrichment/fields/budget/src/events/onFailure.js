8cd0c44715758ffd52d3bced5e6540e309f22b6b
Kalin Chernev <kalin.chernev@gmail.com>
2018-11-14 15:42:35 +0200

    Add backoff strategy in onFailure handler

--- services/enrichment/fields/budget/src/events/onFailure.js
+++ services/enrichment/fields/budget/src/events/onFailure.js
@@ -1,7 +1,8 @@
 import AWS from 'aws-sdk'; // eslint-disable-line import/no-extraneous-dependencies
+import backoff from 'backoff';
 import request from 'request-promise-native';
 
-export const handler = async (event, context, callback) => {
+export const handler = (event, context, callback) => {
   const { SERVICE_ENDPOINT } = process.env;
 
   const lambda = new AWS.Lambda({ apiVersion: '2015-03-31' });
@@ -23,20 +24,39 @@ export const handler = async (event, context, callback) => {
   const functionName = functionParts.join('-');
 
   try {
-    // Verify whether enrichment service is available at the moment.
-    const serviceAvailable = await request.get({ url: SERVICE_ENDPOINT });
-    // return callback(e) if not
-    console.log(serviceAvailable);
+    const call = backoff.call(
+      request.get,
+      { url: SERVICE_ENDPOINT },
+      (err, res) => {
+        const project = JSON.parse(eventParsed.Records['0'].Sns.Message);
+        const retryCount = call.getNumRetries;
 
-    // Execute original handler if the service has come back online.
-    const params = {
-      FunctionName: functionName,
-      InvocationType: 'Event',
-      LogType: 'Tail',
-      Payload: Buffer.from(eventInitial, 'utf8'),
-    };
-    const result = await lambda.invoke(params).promise();
-    return callback(null, result);
+        console.log(`${SERVICE_ENDPOINT} was down with ${res.statusCode} code`);
+        console.log(`Retry project ID ${project.project_id}: ${retryCount}`);
+
+        if (err) return callback(err);
+
+        // Execute original handler if the service has come back online.
+        const params = {
+          FunctionName: functionName,
+          InvocationType: 'Event',
+          LogType: 'Tail',
+          Payload: Buffer.from(eventInitial, 'utf8'),
+        };
+
+        return lambda.invoke(params, (lambdaServiceError, data) => {
+          if (lambdaServiceError) callback(lambdaServiceError);
+          return callback(null, data);
+        });
+      }
+    );
+
+    call.retryIf(
+      err => err.status !== 200 || err.status === 500 || err.status === 502
+    );
+    call.setStrategy(new backoff.ExponentialStrategy({ initialDelay: 500 }));
+    call.failAfter(10);
+    return call.start();
   } catch (e) {
     return callback(e);
   }
