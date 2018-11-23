7319eaace174408ea3b81297d79eb3eab54e8e55
Kalin Chernev <kalin.chernev@gmail.com>
2018-11-23 13:08:11 +0200

    refactor(cli): improve deployment workflows - EUBFR-146

--- tools/eubfr-cli/commands/resources/deployResources.js
+++ tools/eubfr-cli/commands/resources/deployResources.js
@@ -4,29 +4,30 @@ const { exec } = require('child_process');
 
 const shell = promisify(exec);
 
-// Utilities
 const getServiceLocation = require('../../lib/getServiceLocation');
 
 const deployResources = async () => {
-  const resources = ['resources-raw-storage', 'resources-harmonized-storage'];
+  const resources = [
+    'resources-raw-storage',
+    'resources-harmonized-storage',
+    'resources-elasticsearch',
+  ];
 
-  // Using for-of on purpose for a sequential resolution of promises.
-  // eslint-disable-next-line
-  for (const resource of resources) {
-    const cwd = path.resolve(`${getServiceLocation(resource)}`);
+  await Promise.all(
+    resources.map(async resource => {
+      const cwd = path.resolve(`${getServiceLocation(resource)}`);
 
-    try {
-      console.log(`Deploying ${resource}`);
-      console.time(resource);
-      // Deploy the resource.
-      // eslint-disable-next-line
-      await shell('npx sls deploy', { cwd });
-      console.log(`${resource} has been deployed`);
-      console.timeEnd(resource);
-    } catch (e) {
-      console.error(e);
-    }
-  }
+      try {
+        console.log(`Deploying ${resource}`);
+        console.time(resource);
+        await shell('npx sls deploy', { cwd });
+        console.log(`${resource} has been deployed`);
+        console.timeEnd(resource);
+      } catch (e) {
+        console.error(e);
+      }
+    })
+  );
 };
 
 module.exports = deployResources;
