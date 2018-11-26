1f9a6deb7b9dc2fed8db814070195da2de9af762
Kalin Chernev <kalin.chernev@gmail.com>
2018-11-26 13:14:59 +0200

    Resources: allow use of env vars

--- tools/eubfr-cli/lib/getAllProducers.js
+++ tools/eubfr-cli/lib/getAllProducers.js
@@ -1,3 +1,14 @@
-const config = require('../../../config.json'); // eslint-disable-line import/no-unresolved
-
-module.exports = () => config.demo;
+/**
+ * Returns a list of producers.
+ */
+module.exports = () => {
+  try {
+    // This is the one the developer maintains.
+    // eslint-disable-next-line
+    return require('../../../config.json').demo;
+  } catch (e) {
+    // This one contains defaults committed in the repository and is always there.
+    // eslint-disable-next-line
+    return require('../../../config.example.json').demo;
+  }
+};
