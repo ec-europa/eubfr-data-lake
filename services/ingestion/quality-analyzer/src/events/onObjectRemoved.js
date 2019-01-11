dd059c3a998c0b55f7ec95997642b25479d7e5aa
Kalin Chernev <kalin.chernev@gmail.com>
2019-01-11 15:47:05 +0200

    Forgotten versions

--- services/ingestion/quality-analyzer/src/events/onObjectRemoved.js
+++ services/ingestion/quality-analyzer/src/events/onObjectRemoved.js
@@ -31,7 +31,7 @@ export const handler = (event, context, callback) => {
   // elasticsearch client configuration
   const options = {
     host: `https://${API}`,
-    apiVersion: '6.0',
+    apiVersion: '6.3',
     connectionClass,
     index: INDEX,
   };
