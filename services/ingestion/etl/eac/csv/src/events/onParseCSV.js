d3c84db78aea32cd779e6fe00dbe400f52743104
Kalin Chernev <kalin.chernev@gmail.com>
2019-01-21 15:10:11 +0200

    Ignore erroneous records

--- services/ingestion/etl/eac/csv/src/events/onParseCSV.js
+++ services/ingestion/etl/eac/csv/src/events/onParseCSV.js
@@ -46,7 +46,7 @@ export const handler = async (event, context, callback) => {
    */
 
   // Parse
-  const parser = parse({ columns: true });
+  const parser = parse({ columns: true, skip_lines_with_error: true });
 
   // Transform
   const transformer = transform(
