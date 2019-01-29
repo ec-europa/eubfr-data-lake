c1d5ac4942574098a0f6edd3ce271d7abefededa
Kalin Chernev <kalin.chernev@gmail.com>
2019-01-29 17:06:19 +0200

    Implement the logic

--- tools/eubfr-cli/lib/getContentFiles.js
+++ tools/eubfr-cli/lib/getContentFiles.js
@@ -1,4 +1,5 @@
 const AWS = require('aws-sdk');
+const fs = require('fs');
 
 /**
  * Utility to sync an S3 content repository to a local file system.
@@ -8,10 +9,53 @@ const getContentFiles = async ({ folder, producer }) => {
   const { EUBFR_CONTENT_REPOSITORY: repo } = process.env;
 
   const s3 = new AWS.S3();
-  const files = await s3.listObjects({ Bucket: repo }).promise();
 
-  console.log({ folder, producer });
-  console.log(files);
+  try {
+    // Ensure the folder where we are going to place the files exists.
+    if (!fs.existsSync(folder)) {
+      fs.mkdirSync(folder);
+    }
+
+    const results = await s3.listObjects({ Bucket: repo }).promise();
+    let files = results.Contents;
+
+    // If there's a preference for a specific producer, respect it.
+    if (producer !== '*') {
+      files = files.filter(file => file.Key.includes(producer));
+    }
+
+    return Promise.all(
+      files.map(file => {
+        return new Promise((resolve, reject) => {
+          const target = file.Key;
+
+          // Check whether the target object has an extension.
+          // If it doesn't, then it's a folder.
+          if (!target.match(/\.[0-9a-z]+$/i)) {
+            // Ensure the folder existis.
+            if (!fs.existsSync(`${folder}/${target}`)) {
+              fs.mkdirSync(`${folder}/${target}`);
+              // Nothing more to do if it's a folder.
+              resolve();
+            }
+          } else {
+            const localFile = fs.createWriteStream(`${folder}/${target}`);
+
+            s3.getObject({ Bucket: repo, Key: target })
+              .createReadStream()
+              .on('error', error => reject(error))
+              .pipe(localFile)
+              .on('close', () => {
+                console.log(`Downloaded: ${folder}/${target}`);
+                resolve();
+              });
+          }
+        });
+      })
+    );
+  } catch (error) {
+    throw new Error('Issue while trying to download files.', error);
+  }
 };
 
 module.exports = getContentFiles;
