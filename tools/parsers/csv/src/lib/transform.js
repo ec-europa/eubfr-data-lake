c70633fe111003c51a68b8567f2858920bbd3d04
Kalin Chernev <kalin.chernev@gmail.com>
2019-02-05 15:15:07 +0100

    Remove parsers

--- tools/parsers/csv/src/lib/transform.js
+++
@@ -1,8 +0,0 @@
-// Placeholder, please add your actual and latest code.
-const transform = record => {
-  // Uncomment to inspect the row.
-  //   console.log(record);
-  return record;
-};
-
-export default transform;
