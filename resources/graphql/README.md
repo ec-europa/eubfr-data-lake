f7661e64387d6e47f6f3b0d978a67e58f1530063
Kalin Chernev <kalin.chernev@gmail.com>
2019-02-08 16:22:56 +0100

    Refactoring

--- resources/graphql/README.md
+++ resources/graphql/README.md
@@ -4,21 +4,27 @@ A layer on top of Elasticsearch to facilitate focused queries.
 
 ## Deployment
 
-Because the only dependee of this resource is the Elasticsearch domain and endpoint, you can start off easily without deploying any services or demo dashboards:
+Because the only dependee of this resource is the Elasticsearch domain and endpoint, you can start off easily without deploying any services or demo dashboards.
+
+If you are in the project root folder:
 
 ```sh
 $ yarn deploy:resources
 ```
 
-## Flag local
+## Preparations
+
+Before being able to work with the API locally, inside the folder of `@eubfr/resources-graphql` please run the following:
 
 ```sh
-$ export IS_LOCAL=true
+$ yarn run prepare
 ```
 
-This should be automatically set by the serverless framework when functions are invoked locally, but the serverless offline plugin seems to miss that.
+This will deploy the service and its resources on AWS (if not yet done in the previous step) and then export the necessary environment variables locally to facilitate the work with the `serverless-offline` plugin.
 
-## Start server
+## Start
+
+Once the preparation is done, simply run:
 
 ```sh
 $ yarn start
--- resources/graphql/src/api/graphql.js
+++ resources/graphql/src/api/graphql.js
@@ -1,11 +1,40 @@
+import elasticsearch from 'elasticsearch';
+import connectionClass from 'http-aws-es';
+import { GraphQLSchema, GraphQLObjectType } from 'graphql';
+import { composeWithElastic } from 'graphql-compose-elasticsearch';
 import { ApolloServer } from 'apollo-server-lambda';
-import schema from './schema';
 
-const { API_ID, STAGE, REGION, IS_LOCAL } = process.env;
+import getProjectMapping from '../../../elasticsearch/mappings/project';
+
+const projectMapping = getProjectMapping();
+
+const { API_ID, API, INDEX, TYPE, STAGE, REGION, IS_LOCAL } = process.env;
 
 const endpoint = IS_LOCAL
   ? 'http://localhost:4000/graphql'
   : `https://${API_ID}.execute-api.${REGION}.amazonaws.com/${STAGE}/graphql`;
+
+const ProjectTC = composeWithElastic({
+  graphqlTypeName: 'Project',
+  elasticIndex: INDEX,
+  elasticType: TYPE,
+  elasticMapping: projectMapping.mappings.project,
+  elasticClient: new elasticsearch.Client({
+    host: `https://${API}`,
+    connectionClass,
+    apiVersion: '6.3',
+    trace: true,
+  }),
+});
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+      projects: ProjectTC.getResolver('search').getFieldConfig(),
+    },
+  }),
+});
 
 const server = new ApolloServer({
   schema,
