c81b50398e8cd0e69579d3a23e1249e73d56782f
Kalin Chernev <kalin.chernev@gmail.com>
2019-03-18 14:19:11 +0200

    Internal page

--- demo/dashboard-consolidation/src/pages/jobs/Page.js
+++ demo/dashboard-consolidation/src/pages/jobs/Page.js
@@ -1,14 +1,76 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import Highlight from 'react-highlight';
+import 'highlight.js/styles/a11y-light.css';
 
-const JobPage = ({ match }) => {
-  return <div>internal page {JSON.stringify(match)}</div>;
+import clients from '../../clientFactory';
+import indices from '../../clientFactory/esIndices';
+
+class Page extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      job: {},
+    };
+
+    this.clients = null;
+    this.getJob = this.getJob.bind(this);
+  }
+
+  componentDidMount() {
+    this.clients = clients.Create();
+    this.getJob();
+  }
+
+  getJob() {
+    const { match } = this.props;
+    const { id } = match.params;
+
+    return this.clients.public
+      .get({
+        index: indices.jobs,
+        type: 'job',
+        id,
+      })
+      .then(data => {
+        const job = data._source ? data._source : {};
+
+        this.setState({ job });
+      })
+      .catch(error => {
+        console.error(`An error occured: ${error.message}`);
+      });
+  }
+
+  render() {
+    const { job } = this.state;
+    console.log(job);
+    const hasDuplicates = job['CONTAINS DUPLICATES ?'] || '';
+    const sameComputedKey = job['SAME JSON? (computed key)'] || '';
+    const javaJob = job['JAVA JOB RESULT '] ? job['JAVA JOB RESULT '] : '';
+    const kibanaLinks = job['KIBANA LINKS TO SAVED SEARCHES']
+      ? job['KIBANA LINKS TO SAVED SEARCHES']
+      : '';
+
+    return job['JAVA JOB RESULT '] ? (
+      <Fragment>
+        <div>Has duplicates: {hasDuplicates}</div>
+        <div>Same JSON (computed key): {sameComputedKey}</div>
+        <br />
+        <div>Result to evaluate:</div>
+        <Highlight>{javaJob}</Highlight>
+        <div>Kibana links:</div>
+        <Highlight>{kibanaLinks}</Highlight>
+      </Fragment>
+    ) : (
+      'Loading ...'
+    );
+  }
+}
+
+Page.propTypes = {
+  match: PropTypes.object,
 };
 
-JobPage.propTypes = {
-  match: PropTypes.shape({
-    url: PropTypes.string,
-  }),
-};
-
-export default JobPage;
+export default Page;
