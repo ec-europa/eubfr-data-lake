731057f9a2a88f0e4f19f32eaf4ba3bca22dce8b
Kalin Chernev <kalinchernev@users.noreply.github.com>
2019-04-24 16:01:57 +0300

    feat(etls): add interreg 2014TC16RFCB014 - EUBFR-258 (#203)
    * feat(etls): add interreg 2014TC16RFCB014 - EUBFR-258
    
    * Update handler and stub
    
    * Update transform function
    
    * Correction

--- docs/types/README.md
+++ docs/types/README.md
@@ -11,6 +11,7 @@ The EUBFR project uses several types which aim to make the results of ETLs data 
 Here's a list of the transformations made in ETLs around the `Project` model.
 
 - [2014tc16m4tn002 - XLS](./etls/2014tc16m4tn002-xls.md)
+- [2014tc16rfcb014 - CSV](./etls/2014tc16rfcb014-csv.md)
 - [2014tc16rfpc001 - XLS](./etls/2014tc16rfpc001-xls.md)
 - [2014tc16rftn002 - XLS](./etls/2014tc16rftn002-xls.md)
 - [bulgaria - XLS](./etls/bulgaria-xls.md)
--- tools/eubfr-cli/lib/getServices.js
+++ tools/eubfr-cli/lib/getServices.js
@@ -10,6 +10,7 @@ const allServices = [
   { service: 'ingestion-dead-letter-queue', exportEnv: false },
   { service: 'ingestion-quality-analyzer', exportEnv: false },
   { service: 'ingestion-etl-2014tc16m4tn002-xls', exportEnv: false },
+  { service: 'ingestion-etl-2014tc16rfcb014-csv', exportEnv: false },
   { service: 'ingestion-etl-2014tc16rfpc001-xls', exportEnv: false },
   { service: 'ingestion-etl-2014tc16rftn002-xls', exportEnv: false },
   { service: 'ingestion-etl-bulgaria-xls', exportEnv: false },
