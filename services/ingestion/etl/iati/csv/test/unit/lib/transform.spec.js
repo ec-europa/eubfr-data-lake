0aaedb854cfe6e2479733d6a7c4469cb2b6807bf
Kalin Chernev <kalin.chernev@gmail.com>
2019-05-16 10:47:36 +0300

    ETL iati

--- services/ingestion/etl/iati/csv/test/unit/lib/transform.spec.js
+++ services/ingestion/etl/iati/csv/test/unit/lib/transform.spec.js
@@ -20,6 +20,11 @@ describe('DG IATI CSV transformer', () => {
     expect(result).toMatchSnapshot();
   });
 
+  test('Budget values do not contain decimals', () => {
+    expect(result.budget.eu_contrib.value % 1).toBe(0);
+    expect(result.budget.total_cost.value % 1).toBe(0);
+  });
+
   test('Project location information is correctly mapped', () => {
     const firstLocation = result.project_locations[0];
     expect(firstLocation).toMatchSnapshot();
