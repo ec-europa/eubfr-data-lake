2d5d0691cbb92072d7be2820e60a4ff0ee1ec28f
Kalin Chernev <kalin.chernev@gmail.com>
2019-05-24 17:43:03 +0300

    Add separate stub for dates

--- services/ingestion/etl/2014tc16rfcb021/xls/test/unit/lib/transform.spec.js
+++ services/ingestion/etl/2014tc16rfcb021/xls/test/unit/lib/transform.spec.js
@@ -4,12 +4,15 @@
 
 import mapper from '../../../src/lib/transform';
 import testRecord from '../../stubs/record.json';
+import testRecordDates from '../../stubs/recordDates.json';
 
 describe('2014tc16rfcb021 XLS transformer', () => {
   let result = {};
+  let resultDates = {};
 
   beforeAll(() => {
     result = mapper(testRecord);
+    resultDates = mapper(testRecordDates);
   });
 
   test('Returns null when record is not provided', () => {
@@ -28,5 +31,10 @@ describe('2014tc16rfcb021 XLS transformer', () => {
     const resultCopy = mapper(copy);
     expect(resultCopy.timeframe.from).toBe('2016-02-03T00:00:00.000Z');
     expect(resultCopy.timeframe.to).toBe('2018-11-28T21:00:00.000Z');
+  });
+
+  test('Produces correct JSON output in case of Dates in an ISO format', () => {
+    console.log(resultDates);
+    expect(resultDates).toMatchSnapshot();
   });
 });
