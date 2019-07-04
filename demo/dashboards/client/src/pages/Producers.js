651ac86468b6e126eb4dd1c84d32f255255c4bf2
Kalin Chernev <kalin.chernev@gmail.com>
2019-07-04 16:02:21 +0300

    List existing ETLs

--- demo/dashboards/client/src/pages/Producers.js
+++ demo/dashboards/client/src/pages/Producers.js
@@ -1,17 +1,30 @@
 import React from 'react';
+
+import Spinner from '../components/Spinner';
 import handleErrors from '../lib/handleErrors';
 
 class Producers extends React.Component {
+  state = {
+    isLoading: true,
+    dashboards: [],
+  };
+
   componentDidMount() {
-    const { REACT_APP_DASHBOARDS_SERVER } = process.env;
-    const endpoint = `https://${REACT_APP_DASHBOARDS_SERVER}/producers`;
+    const { REACT_APP_DASHBOARDS_SERVER, NODE_ENV } = process.env;
+    const resource = 'producers';
+
+    const endpoint =
+      NODE_ENV === 'development'
+        ? 'http://localhost:4000'
+        : `https://${REACT_APP_DASHBOARDS_SERVER}`;
 
     window
-      .fetch(endpoint)
+      .fetch(`${endpoint}/${resource}`)
       .then(handleErrors)
       .then(response => response.json())
       .then(data => {
-        console.log(data);
+        const { dashboards } = data.data;
+        this.setState({ dashboards, isLoading: false });
       })
       .catch(error => {
         console.error(`An error occured: ${error.message}`);
@@ -19,7 +32,37 @@ class Producers extends React.Component {
   }
 
   render() {
-    return <div>Producers list.</div>;
+    const { dashboards, isLoading } = this.state;
+
+    return (
+      <>
+        <p>Available ETLs</p>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <ul>
+            {dashboards.map((dashboard, key) => {
+              const name = dashboard.Name;
+              const producerParts = name.split('-');
+              const producer = producerParts.pop();
+
+              return (
+                <li key={key}>
+                  <a
+                    className="ecl-link ecl-link--external"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href={`http://${name}.s3-website.eu-central-1.amazonaws.com`}
+                  >
+                    {producer}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        )}
+      </>
+    );
   }
 }
 
--- demo/dashboards/server/src/api/producers.js
+++ demo/dashboards/server/src/api/producers.js
@@ -1,14 +1,31 @@
+import AWS from 'aws-sdk'; // eslint-disable-line import/no-extraneous-dependencies
+
 export const handler = async () => {
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*', // Required for CORS support
-      'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
-    },
-    body: JSON.stringify({
-      message: 'List existing producers',
-    }),
-  };
+  const { EUBFR_STAGE: stage } = process.env;
+  const s3 = new AWS.S3();
+
+  try {
+    const results = await s3.listBuckets().promise();
+    const buckets = results.Buckets;
+
+    const dashboards = buckets.filter(bucket =>
+      bucket.Name.includes(`eubfr-${stage}-demo-dashboard-client-`)
+    );
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Required for CORS support
+        'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
+      },
+      body: JSON.stringify({ data: { dashboards } }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: error }),
+    };
+  }
 };
 
 export default handler;
