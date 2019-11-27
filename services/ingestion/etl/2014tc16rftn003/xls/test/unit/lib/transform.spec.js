970203328a8cebffdef1901940fb73bbd530b8bd
Kalin Chernev <kalinchernev@users.noreply.github.com>
2019-11-27 08:47:02 +0100

    Correct third_parties (#228)

--- services/ingestion/etl/2014tc16rftn003/xls/test/unit/lib/transform.spec.js
+++ services/ingestion/etl/2014tc16rftn003/xls/test/unit/lib/transform.spec.js
@@ -19,4 +19,16 @@ describe('2014tc16rftn003 XLS transformer', () => {
   test('Produces correct JSON output structure', () => {
     expect(result).toMatchSnapshot();
   });
+
+  test('In field `third_parties`, respect countries of several words', () => {
+    const copy = JSON.parse(JSON.stringify(testRecord));
+
+    copy.Country =
+      'FI Finland / SUOMI / FINLAND;UK United Kingdom / UNITED KINGDOM; NRC Not a real country / NONEXISTENT';
+    const copyResult = mapper(copy);
+
+    expect(copyResult.third_parties[0].country).toBe('Finland');
+    expect(copyResult.third_parties[1].country).toBe('United Kingdom');
+    expect(copyResult.third_parties[2].country).toBe('Not a real country');
+  });
 });
